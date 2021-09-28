7b7d45705bd99efbdf1827626df2f16d0ab229c1
Zhenyu Wei <zhenyuwei99@gmail.com>
2021-09-28 13:32:36 +0800

    mdpy.error.UnitDimensionDismatchedError and mdpy.error.ChangeDeviceBoundedDataError

--- mdpy/error.py
+++ mdpy/error.py
@@ -9,3 +9,18 @@ contact : zhenyuwei99@gmail.com
 copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
 '''
 
+class UnitDimensionDismatchedError(Exception):
+    '''
+    This error occurs when the base dimension of two quantities is dismatched for a specific operation.
+    Used in:
+    - mdpy.unit.base_dimension
+    '''
+    pass
+
+class ChangeDeviceBoundedDataError(Exception):
+    '''
+    This error occurs when users try to change the immutable jax.DeviceArray
+    Used in:
+    - mdpy.unit.quantity
+    '''
+    pass
