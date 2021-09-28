1734776cc940052cbc092e24d757e28f0962018d
Zhenyu Wei <zhenyuwei99@gmail.com>
2021-09-28 18:43:37 +0800

    Update test for mdpy.core.Particle

--- mdpy/test/test_particle.py
+++ mdpy/test/test_particle.py
@@ -31,38 +31,8 @@ class TestParticle:
         assert particle.mass == None
 
     def test_exceptions(self):
-        with pytest.raises(SpatialDimError):
-            Particle(1, 'CA', position=[1, 2, 3, 4])
-
-        with pytest.raises(SpatialDimError):
-            Particle(1, 'CA', velocity=[1, 2, 3, 4])
-
-    def test_position(self):
-        particle = Particle(1, 'CA', position=[1, 2, 3])
-        assert particle.position.unit == default_length_unit
-        assert particle.position[0].value == 1
-
-        particle = Particle(1, 'CA', position=np.ones([3, 1]))
-        assert len(list(particle.position.value.shape)) == 1
-
-        particle.position = [3, 2, 1]
-        assert particle.position[0].value == 3
-        assert particle.position.unit == default_length_unit
-        assert len(list(particle.position.value.shape)) == 1
-
-    def test_velocity(self):
-        particle = Particle(1, 'CA', velocity=[1, 2, 3])
-        assert particle.velocity.unit == default_length_unit / default_time_unit
-        assert particle.velocity[0].value == 1
-
-        particle = Particle(1, 'CA', velocity=np.ones([3, 1]))
-        assert len(list(particle.velocity.value.shape)) == 1
-
-        particle.velocity = [3, 2, 1]
-        assert particle.velocity[0].value == 3
-        assert particle.velocity.unit == default_length_unit / default_time_unit
-        assert len(list(particle.velocity.value.shape)) == 1
-
+        pass 
+    
     def test_mass(self):
         particle = Particle(1, 'CA', mass=1)
         assert particle.mass.value == 1
