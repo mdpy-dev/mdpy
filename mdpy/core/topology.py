22ac0e77df5377238bdb37c0007c9a3381b86dfa
Zhenyu Wei <zhenyuwei99@gmail.com>
2021-09-29 23:31:16 +0800

    Remove redundant import of mdpy.core.topology

--- mdpy/core/topology.py
+++ mdpy/core/topology.py
@@ -9,7 +9,6 @@ contact : zhenyuwei99@gmail.com
 copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
 '''
 
-from _pytest.python_api import raises
 from . import Particle
 from ..error import *
 
