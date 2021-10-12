2b6754b75be44519ad8152394641d6fd73f461e9
Zhenyu Wei <zhenyuwei99@gmail.com>
2021-10-12 16:01:49 +0800

    Update mdpy.file.PDBFile: Add pbc_matrix attributes

--- mdpy/file/pdb_file.py
+++ mdpy/file/pdb_file.py
@@ -19,8 +19,9 @@ from mdpy.core.particle import Particle
 class PDBFile:
     def __init__(self, file_path) -> None:
         # Initial reader and parser setting
-        self._reader = PDBReader(file_path)
-        self._parser = PDBParser(file_path).parse()
+        self._file_path = file_path
+        self._reader = PDBReader(self._file_path)
+        self._parser = PDBParser(self._file_path).parse()
         # Parse data
         self._num_particles = self._parser.n_atoms
         self._particle_ids = list(self._parser.ids.values)
@@ -35,6 +36,7 @@ class PDBFile:
             self._molecule_types.append(molecule_types[resid])
         self._chain_ids = list(self._parser.chainIDs.values)
         self._positions = self._reader.ts.positions
+        self._pbc_matrix = self._reader.ts.triclinic_dimensions
 
     def create_particles(self):
         particles = []
@@ -99,3 +101,7 @@ class PDBFile:
     @property
     def positions(self):
         return self._positions
+
+    @property
+    def pbc_matrix(self):
+        return self._pbc_matrix
