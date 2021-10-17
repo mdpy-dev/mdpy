0fab160a26084fc34b770bdb9c92c5df0332fb8e
Zhenyu Wei <zhenyuwei99@gmail.com>
2021-10-17 17:19:19 +0800

    Update use range for mdpy.error.PBCPoorDefinedError

--- mdpy/error.py
+++ mdpy/error.py
@@ -91,7 +91,7 @@ class PBCPoorDefinedError(Exception):
     - Two or more column vector in pbc_matrix is linear corellated
     
     Used in:
-    - mdpy.simulation
+    - mdpy.forcefield.charmm_forcefield
     '''
     pass
 
