45c2472b2b4dbe5c3ae976ba22198919d2956044
Zhenyu Wei <zhenyuwei99@gmail.com>
2021-10-23 23:04:20 +0800

    Update test for mdpy.core.Particle class

--- mdpy/test/test_particle.py
+++ mdpy/test/test_particle.py
@@ -54,11 +54,11 @@ class TestParticle:
         particle = Particle(particle_id=1, particle_type='C', matrix_id=0)
         particle.add_bonded_particle(1)
         assert particle.num_bonded_particles == 1
-        assert particle.bonded_particle[0] == 1
+        assert particle.bonded_particles[0] == 1
 
         particle.add_bonded_particle(2)
         assert particle.num_bonded_particles == 2
-        assert particle.bonded_particle[1] == 2
+        assert particle.bonded_particles[1] == 2
 
         with pytest.raises(ParticleConflictError):
             particle.add_bonded_particle(1)
