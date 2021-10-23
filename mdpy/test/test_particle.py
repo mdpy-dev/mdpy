d4da17953e0e48d4721567bdb0111eeefbfc1d9c
Zhenyu Wei <zhenyuwei99@gmail.com>
2021-10-23 22:38:35 +0800

    Update test for mdpy.core.Particle class

--- mdpy/test/test_particle.py
+++ mdpy/test/test_particle.py
@@ -49,3 +49,19 @@ class TestParticle:
 
         with pytest.raises(UnitDimensionDismatchedError):
             Particle(charge=Quantity(1, default_energy_unit))
+
+    def test_bonded_particles(self):
+        particle = Particle(particle_id=1, particle_type='C', matrix_id=0)
+        particle.add_bonded_particles(1)
+        assert particle.num_bonded_particles == 1
+        assert particle.bonded_particle[0] == 1
+
+        particle.add_bonded_particles(2)
+        assert particle.num_bonded_particles == 2
+        assert particle.bonded_particle[1] == 2
+
+        with pytest.raises(ParticleConflictError):
+            particle.add_bonded_particles(1)
+
+        with pytest.raises(ParticleConflictError):
+            particle.add_bonded_particles(0)
