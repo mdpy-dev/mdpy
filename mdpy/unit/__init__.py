54b59d5994c1a3502d971b8d7f2d45c5dfa4e28a
Zhenyu Wei <zhenyuwei99@gmail.com>
2021-11-04 19:05:10 +0800

    Update mdpy.unit package: Add UNIT_PRESION and QUANTITY_PRESISION

--- mdpy/unit/__init__.py
+++ mdpy/unit/__init__.py
@@ -4,6 +4,10 @@ __email__ = "zhenyuwei99@gmail.com"
 __copyright__ = "Copyright 2021-2021, Southeast University and Zhenyu Wei"
 __license__ = "GPLv3"
 
+
+UNIT_PRECISION = 1e-8
+QUANTITY_PRECISION = 1e-8
+
 from .base_dimension import BaseDimension
 from .unit import Unit
 
