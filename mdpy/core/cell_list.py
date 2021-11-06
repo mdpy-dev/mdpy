1b1172903327e0a6af93c60b9e785cf396ca60f2
Zhenyu Wei <zhenyuwei99@gmail.com>
2021-11-07 00:42:53 +0800

    Update mdpy.core.CellList class: 1. Add update update method. 2. Add kernel function to increase performance

--- mdpy/core/cell_list.py
+++ mdpy/core/cell_list.py
@@ -10,6 +10,7 @@ copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
 '''
 
 import numpy as np
+import numba as nb
 from .. import SPATIAL_DIM, env
 from ..math import *
 from ..unit import *
@@ -19,38 +20,16 @@ class CellList:
     def __init__(self) -> None:
         self._cutoff_radius = env.NUMPY_FLOAT(0)
         self._pbc_diag = np.zeros(SPATIAL_DIM, env.NUMPY_FLOAT)
+        self._num_particles = 0
+        self._particle_cell_index = None # N x 3
+        self._cell_list = None # n x n x n
+        self._kernel = nb.njit(
+            (env.NUMBA_FLOAT[:, ::1], env.NUMBA_FLOAT[:, ::1], env.NUMBA_INT[::1])
+        )(self.kernel)
 
-    def _get_cell_index(self, x: int, y: int, z: int):
-        x -= self._cell_id_min[0]
-        y -= self._cell_id_min[1]
-        z -= self._cell_id_min[2]
-        cell_index = [int(x), int(y), int(z)]
-        for i, j in enumerate(cell_index):
-            if j < 0:
-                cell_index[i] = self._num_cells_vec[i] + j
-            elif j >= self._num_cells_vec[i]:
-                cell_index[i] -= self._num_cells_vec[i]
-        x, y, z = cell_index
-        index = x * self._num_cells_vec[1] * self._num_cells_vec[2] + y * self._num_cells_vec[2] + z       
-        return index
-
-    def __getitem__(self, keys):
-        x, y, z = keys
-        return self._cell_list[self._get_cell_index(x, y, z)]
-
-    def get_neighbors(self, position: np.ndarray):
-        if self._num_cells <= 27:
-            return self._particles
-        else:
-            matrix_ids = []
-            cell_id = np.floor(np.dot(position, self._cell_inv))
-            for i in range(-1, 2):
-                for j in range(-1, 2):
-                    for k in range(-1, 2):
-                        matrix_ids.extend(
-                            self[cell_id[0]+i, cell_id[1]+j, cell_id[2]+k]
-                        )
-        return matrix_ids
+    def __getitem__(self, matrix_id):
+        x, y, z = self._particle_cell_index[matrix_id, :]
+        return self._cell_list[x, y, z, :]
 
     def _is_poor_defined(self, verbose=False):
         if self._cutoff_radius == 0:
@@ -71,19 +50,17 @@ class CellList:
 
     def _update_attributes(self):
         self._cell_matrix = np.ones(SPATIAL_DIM) * self._cutoff_radius
-        self._num_cells_vec = np.floor(self._pbc_diag / self._cell_matrix)
+        self._num_cells_vec = np.floor(self._pbc_diag / self._cell_matrix).astype(env.NUMPY_INT)
         for i in self._num_cells_vec:
             if i == 0:
                 raise CellListPoorDefinedError(
-                    'The cutoff_radius is too large compared to the pbc box size'
+                    'The cutoff_radius is too large to create cell list'
                 )
+        # Construct at least 27 cells
+        self._num_cells_vec[self._num_cells_vec < 3] = 3
         self._num_cells = env.NUMPY_INT(np.prod(self._num_cells_vec))
-        self._cell_matrix = np.diag(self._pbc_diag / self._num_cells_vec)
+        self._cell_matrix = np.diag(self._pbc_diag / self._num_cells_vec).astype(env.NUMPY_FLOAT)
         self._cell_inv = np.linalg.inv(self._cell_matrix)
-        self._num_particles = 0
-        self._num_cells_vec = [int(i) for i in self._num_cells_vec]
-        self._cell_list = [[] for _ in range(self._num_cells)]
-        self._cell_id_min = np.array([0, 0, 0])
 
     def set_pbc_matrix(self, pbc_matrix: np.ndarray):
         self._pbc_diag = pbc_matrix.diagonal()
@@ -97,20 +74,41 @@ class CellList:
 
     def update(self, positions: np.ndarray):
         if not self._is_poor_defined():
-            self._cell_list = [[] for _ in range(self._num_cells)]
-            cell_id = np.floor(np.dot(positions, self._cell_inv))
-            self._num_particles = cell_id.shape[0]
-            self._particles = list(range(self._num_particles))
-            self._cell_id_min = cell_id.min(0)
-            for particle in range(self._num_particles):
-                self._cell_list[
-                    self._get_cell_index(*cell_id[particle])
-                ].append(particle)
+            # Set the position to positive value
+            # Ensure the id calculated by matrix dot corresponds to the cell_list index
+            positive_position = positions - positions.min(0) 
+            self._particle_cell_index, self._cell_list = self._kernel(
+                positive_position, self._cell_inv, self._num_cells_vec
+            )
         else:
             self._is_poor_defined(verbose=True)
-            raise CellListPoorDefinedError(
-                'Cell list is poorly defined, please check CellList._is_poor_deined(verbose=True)'
-            )
+            raise CellListPoorDefinedError('Cell list is poorly defined.')
+
+    @staticmethod
+    def kernel(positions: np.ndarray, cell_inv: np.ndarray, num_cell_vec: np.ndarray):
+        # Read input
+        int_type = num_cell_vec.dtype
+        num_particles = positions.shape[0]
+        # Set variables
+        num_x, num_y, num_z = num_cell_vec
+        num_cell_particles = np.zeros((num_x, num_y, num_z), dtype=int_type) # Number of particles in each cells
+        # Assign particles
+        particle_cell_index = np.floor(np.dot(positions, cell_inv)).astype(int_type) # The cell index of each particle
+        for particle in range(num_particles):
+            x, y, z = particle_cell_index[particle]
+            num_cell_particles[x, y, z] += 1
+        # Build cell list
+        max_num_cell_particles = num_cell_particles.max() # The number of particles of cell that contain the most particles
+        cell_list = np.ones((
+            num_cell_vec[0], num_cell_vec[1], 
+            num_cell_vec[2], max_num_cell_particles
+        ), dtype=int_type) * -1
+        cur_cell_flag = np.zeros_like(num_cell_particles)
+        for particle in range(num_particles):
+            x, y, z = particle_cell_index[particle]
+            cell_list[x, y, z, cur_cell_flag[x, y, z]] = particle
+            cur_cell_flag[x, y, z] += 1
+        return particle_cell_index.astype(int_type), cell_list.astype(int_type)
 
     @property
     def cutoff_radius(self):
@@ -127,3 +125,15 @@ class CellList:
     @property
     def cell_inv(self):
         return self._cell_inv
+
+    @property
+    def particle_cell_index(self):
+        return self._particle_cell_index
+
+    @property
+    def cell_list(self):
+        return self._cell_list
+
+    @property
+    def num_cell_vec(self):
+        return self._num_cells_vec
