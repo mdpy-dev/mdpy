0c5ec76d415a51d360593428e5660f4a92b92039
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-01-09 16:40:18 +0800

    Add mdpy.minimizer.minimizer.Minimizer class as the base class of mdpy.minimizer package

---
+++ mdpy/minimizer/minimizer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+file : minimizer.py
+created time : 2022/01/09
+author : Zhenyu Wei
+version : 1.0
+contact : zhenyuwei99@gmail.com
+copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
+'''
+
+from ..ensemble import Ensemble
+from ..unit import *
+
+class Minimizer:
+    def __init__(
+        self, output_unit=kilojoule_permol, 
+        output_unit_label='kj/mol',
+        is_verbose=False
+    ) -> None:
+        self._output_unit = output_unit
+        self._output_unit_label = output_unit_label
+        self._is_verbose = is_verbose
+
+    def minimize(self, ensemble: Ensemble, energy_tolerance=1e-6, max_iterations: int=1000):
+        raise NotImplementedError(
+            'The subclass of mdpy.minimizer.Minimizer class should overload minimize method'
+        )
+
+    def _energy2str(self, energy: Quantity):
+        return '%.5f %s' %(
+            Quantity(energy, default_energy_unit).convert_to(self._output_unit).value,
+            self._output_unit_label
+        )
