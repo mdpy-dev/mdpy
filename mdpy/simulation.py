0d448297d58426dd52d517c3eadcb08fdbd83dfe
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-01-09 16:43:10 +0800

    Update mdpy.Simulation class: 1. Update __init__ method, adding minimizer as argument, 2. Add set_minimizer method for change minimizer, 3. Replace minimize_energy with minimize method

--- mdpy/simulation.py
+++ mdpy/simulation.py
@@ -14,14 +14,18 @@ import numpy as np
 from mdpy.math.pbc import wrap_positions
 from .ensemble import Ensemble
 from .integrator import Integrator
+from .minimizer import Minimizer
+from .minimizer import *
 from .error import *
 from .unit import *
 
+default_minimizer = SteepestDescentMinimizer(kilojoule_permol, 'kj/mol', is_verbose=False)
 
 class Simulation:
-    def __init__(self, ensemble: Ensemble, integrator: Integrator) -> None:
+    def __init__(self, ensemble: Ensemble, integrator: Integrator, minimizer: Minimizer=default_minimizer) -> None:
         self._ensemble = ensemble
         self._integrator = integrator
+        self._minimizer = minimizer
         self._cur_step = 0
         self._dumpers = []
         self._minimum_dump_frequency = 0
@@ -37,6 +41,9 @@ class Simulation:
                 self._minimum_dump_frequency, dumper.dump_frequency
             )
 
+    def set_minimizer(self, minimizer: Minimizer):
+        self._minimizer = Minimizer
+
     def _dump(self):
         for dumper in self._dumpers:
             if self._cur_step % dumper.dump_frequency == 0:
@@ -45,51 +52,21 @@ class Simulation:
     def reset_simulation_step(self):
         self._cur_step = 0
 
+    def minimize(self, energy_tolerance, max_interations):
+        self._minimizer.minimize(self._ensemble, energy_tolerance, max_interations)
+        self._dump()
+
     def integrate(self, num_steps: int):
         if self._num_dumpers == 0 or self._minimum_dump_frequency == 0:
             raise DumperPoorDefinedError(
                 'No dumper has been added to Simulation yet.'
             )
         target_step = self._cur_step + num_steps
-        self._dump()
         while self._cur_step < target_step:
             self._integrator.integrate(
                 self._ensemble, self._minimum_dump_frequency)
             self._cur_step += self._minimum_dump_frequency
             self._dump()
-
-    def minimize_energy(self, alpha, energy_tolerance=1e-6, max_iterations=1000):
-        energy_convert_unit = default_energy_unit / kilojoule_permol
-        self.ensemble.update()
-        print('Start energy minimization:')
-        print(
-            'Initial potential energy: %.5f kj/mol'
-            % Quantity(self._ensemble.potential_energy, energy_convert_unit).value
-        )
-        self._dump()
-        cur_iteration = 0
-        cur_energy, pre_energy = self._ensemble.potential_energy, self._ensemble.potential_energy
-        while cur_iteration < max_iterations:
-            self.ensemble.state.set_positions(wrap_positions(
-                self._ensemble.state.positions + alpha * self._ensemble.forces,
-                *self._ensemble.state.pbc_info
-            ))
-            self._ensemble.update()
-            cur_energy = self._ensemble.potential_energy
-            energy_error = np.abs((cur_energy - pre_energy) / pre_energy)
-            if energy_error < energy_tolerance:
-                print('Penultimate potential energy: %.5f kj/mol' %
-                      (Quantity(pre_energy, energy_convert_unit).value))
-                print('Final potential energy: %.5f kj/mol' %
-                      (Quantity(cur_energy, energy_convert_unit).value))
-                print('Energy error: %s < %e' %
-                      (energy_error, energy_tolerance))
-                self._dump()
-                return None
-            cur_iteration += 1
-        print('Final potential energy: %.5f kj/mol' %
-              (Quantity(cur_energy, energy_convert_unit).value))
-        self._dump()
 
     @property
     def ensemble(self):
