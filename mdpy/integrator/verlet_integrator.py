7a14560cf7b9e06d1762976c32f490dbeaa4708e
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-01-09 13:49:35 +0800

    Update mdpy.integrator package: Rename sample method to integrate

--- mdpy/integrator/verlet_integrator.py
+++ mdpy/integrator/verlet_integrator.py
@@ -19,7 +19,7 @@ class VerletIntegrator(Integrator):
         super().__init__(time_step)
         self._time_step_square = self._time_step**2
 
-    def sample(self, ensemble: Ensemble, num_steps: int=1):
+    def integrate(self, ensemble: Ensemble, num_steps: int=1):
         # Setting variables
         cur_step = 0
         masses = ensemble.topology.masses
