aedaeb716ba35dcd4a06d126ed24a9774bd2a6e2
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-02-20 00:41:32 +0800

    Add mdpy.analyser.Analyser class

--- mdpy/analyser/__init__.py
+++ mdpy/analyser/__init__.py
@@ -4,3 +4,6 @@ __email__ = "zhenyuwei99@gmail.com"
 __copyright__ = "Copyright 2021-2021, Southeast University and Zhenyu Wei"
 __license__ = "GPLv3"
 
+from .analyser import Analyser
+
+__all__ = []
