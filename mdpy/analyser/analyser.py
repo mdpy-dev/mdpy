4d28f11fe92dbcb67da55c166340f3c85210ec60
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-02-21 21:41:10 +0800

    1. Add mdpy.analyser.anlyser.AnalyserResult class, 2. Update mdpy.analyser.Analyser class: add selction_condition property and setter method

--- mdpy/analyser/analyser.py
+++ mdpy/analyser/analyser.py
@@ -10,12 +10,42 @@ copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
 '''
 
 from ..core import Trajectory
+from ..utils import check_selection_condition
+
+class AnalyserResult:
+    def __init__(self, title: str, description: dict, result: dict) -> None:
+        self._title = title
+        self._description = description
+        self._result = result
+
+    def __repr__(self):
+        return '<mdpy.analyser.AnalyserResult object of %s at %x>' %(self._title, id(self))
+
+    __str__ = __repr__
+
+    @property
+    def title(self):
+        return self._title
+    
+    @property
+    def description(self):
+        descrption = '------------\nDescription of %s\n' %self
+        descrption += 'Title: \n- %s\n' %self._title
+        descrption += 'Keys: \n'
+        for key, value in self._description.items():
+            descrption += '- %s: %s\n' %(key, value)
+        return descrption + '------------'
+
+    @property
+    def result(self):
+        return self._result
 
 class Analyser:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, selection_condition: list[dict]) -> None:
+        check_selection_condition(selection_condition)
+        self._selection_condition = selection_condition
 
-    def analysis(self, trajectory: Trajectory):
+    def analysis(self, trajectory: Trajectory) -> AnalyserResult:
         raise NotImplementedError(
             'The subclass of mdpy.analyser.Analyser class should overload analysis method.'
         )
@@ -24,3 +54,11 @@ class Analyser:
         raise NotImplementedError(
             'The subclass of mdpy.analyser.Analyser class should overload save method.'
         )
+
+    @property
+    def selection_condition(self):
+        return self._selection_condition 
+
+    @selection_condition.setter
+    def selection_condition(self, condition: list[dict]):
+        self._selection_condition = condition
