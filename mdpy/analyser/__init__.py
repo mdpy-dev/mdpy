c54a4dc140f06c66fc3088c541566653c2f984d3
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-02-23 12:18:01 +0800

    Add mdpy.analyser.ResidenceTimeAnalyser (UNFINISHED)

--- mdpy/analyser/__init__.py
+++ mdpy/analyser/__init__.py
@@ -13,9 +13,10 @@ from .mobility_analyser import MobilityAnalyser
 # Thermodynamics properties
 from .rdf_analyser import RDFAnalyser
 from .coordination_number_analyser import CoordinationNumberAnalyser
+from .residence_time_analyser import ResidenceTimeAnalyser
 
 __all__ = [
     'load_analyser_result',
     'DiffusionAnalyser', 'MobilityAnalyser',
-    'RDFAnalyser', 'CoordinationNumberAnalyser'
+    'RDFAnalyser', 'CoordinationNumberAnalyser', 'ResidenceTimeAnalyser'
 ]
