36271a602665b802619d01228eeb775b0665d2dc
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-02-24 20:05:17 +0800

    Update test for mdpy.io.HDF5Parser and mdpy.io.HDF5Writer class

---
+++ mdpy/test/test_hdf5_writer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+file : test_hdf5_writer.py
+created time : 2022/02/24
+author : Zhenyu Wei
+version : 1.0
+contact : zhenyuwei99@gmail.com
+copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
+'''
+
+import os
+import pytest
+import numpy as np
+from ..io import HDF5Writer, PSFParser, PDBParser
+from ..unit import *
+from ..error import *
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+data_dir = os.path.join(cur_dir, 'data')
+out_dir = os.path.join(cur_dir, 'out')
+file_path = os.path.join(out_dir, 'test_hdf5_writer.hdf5')
+
+class TestHDF5Writer:
+    def setup(self):
+        self.topology = PSFParser(os.path.join(data_dir, '6PO6.psf')).topology
+        self.positions = PDBParser(os.path.join(data_dir, '6PO6.pdb')).positions
+        self.pbc_matrix = Quantity(np.diag([1]*3), nanometer)
+
+    def teardown(self):
+        self.topology = None
+        self.positions = None
+        self.pbc_matrix = None
+
+    def test_attributes(self):
+        pass
+
+    def test_exceptions(self):
+        with pytest.raises(FileFormatError):
+            HDF5Writer('te.hd')
+
+    def test_write(self):
+        writer = HDF5Writer(file_path, topology=self.topology, pbc_matrix=self.pbc_matrix)
+        writer.write(self.positions)
+        writer.write(self.positions)
