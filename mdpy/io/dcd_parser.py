eabaaf2d5906a5e60a0e161da9550b93a5d0b98b
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-03-16 21:17:32 +0800

    Update mdpy.io.DCDParser: Add is_parse_all keyword in init for extreme large file to read positions on calling get_positions method

--- mdpy/io/dcd_parser.py
+++ mdpy/io/dcd_parser.py
@@ -11,25 +11,53 @@ copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
 
 import numpy as np
 import MDAnalysis as mda
-from .. import env
-from ..error import *
+from mdpy import env, SPATIAL_DIM
+from mdpy.error import *
 
 class DCDParser:
-    def __init__(self, file_path: str) -> None:
+    def __init__(self, file_path: str, is_parse_all=True) -> None:
         # Initial reader and parser setting
         if not file_path.endswith('.dcd'):
             raise FileFormatError('The file should end with .dcd suffix')
         self._file_path = file_path
+        self._is_parse_all = is_parse_all
         self._reader = mda.coordinates.DCD.DCDReader(self._file_path)
-        if self._reader.n_frames == 1:
-            self._positions = self._reader.ts.positions.astype(env.NUMPY_FLOAT)
-        else:
-            self._positions = [ts.positions.astype(env.NUMPY_FLOAT) for ts in self._reader.trajectory]
-            self._positions = np.stack(self._positions)
+        if self._is_parse_all:
+            if self._reader.n_frames == 1:
+                self._positions = self._reader.ts.positions.astype(env.NUMPY_FLOAT)
+            else:
+                self._positions = [ts.positions.astype(env.NUMPY_FLOAT) for ts in self._reader.trajectory]
+                self._positions = np.stack(self._positions)
         self._pbc_matrix = self._reader.ts.triclinic_dimensions
+
+    def get_positions(self, *frames):
+        num_frames = self._reader.trajectory.n_frames
+        num_particles = self._reader.trajectory.n_atoms
+        num_target_frames = len(frames)
+        if num_target_frames == 1:
+            if frames[0] >= num_frames:
+                raise ArrayDimError(
+                    '%d beyond the number of frames %d stored in dcd file'
+                    %(frames[0], num_frames)
+                )
+            result = self._reader.trajectory[frames[0]].positions.copy().astype(env.NUMPY_FLOAT)
+        else:
+            result = np.zeros([num_target_frames, num_particles, SPATIAL_DIM])
+            for index, frame in enumerate(frames):
+                if frame >= num_frames:
+                    raise ArrayDimError(
+                        '%d beyond the number of frames %d stored in dcd file'
+                        %(frame, num_frames)
+                    )
+                result[index, :, :] = self._reader.trajectory[frame].positions.astype(env.NUMPY_FLOAT)
+        return result
 
     @property
     def positions(self) -> np.ndarray:
+        if not self._is_parse_all:
+            raise ParserPoorDefinedError(
+                'positions property is not supported as `is_parse_all==False`, calling `get_position` method'
+            )
         return self._positions.copy()
 
     @property
