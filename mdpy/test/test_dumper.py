0714aa65bc5052b92428c2a91761a67e50c67a07
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-03-18 14:43:52 +0800

    Update test for mdpy.dumper package

--- mdpy/test/test_dumper.py
+++ mdpy/test/test_dumper.py
@@ -10,7 +10,8 @@ copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
 '''
 
 import pytest, os
-from ..dumper import Dumper
+from mdpy.dumper import Dumper
+from mdpy.error import *
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 out_dir = os.path.join(cur_dir, 'out')
@@ -23,15 +24,18 @@ class TestDumper:
         pass
 
     def test_attributes(self):
-        dumper = Dumper(self.file, 1)
+        dumper = Dumper(self.file, 1, 'txt')
         assert dumper.file_path == os.path.join(out_dir, 'test_dumper.txt')
         assert dumper.dump_frequency == 1
 
     def test_exceptions(self):
-        dumper = Dumper(self.file, 1)
+        dumper = Dumper(self.file, 1, 'txt')
         with pytest.raises(NotImplementedError):
             dumper.dump(1)
 
+        with pytest.raises(FileFormatError):
+            Dumper(self.file, 1, 'tt')
+
     def test_dump_info(self):
-        dumper = Dumper(self.file, 1)
+        dumper = Dumper(self.file, 1, 'txt')
         dumper._dump_info('test dump info')
