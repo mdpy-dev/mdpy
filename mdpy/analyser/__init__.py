a5620bbb28cfc24ec529044bdc66e0c6e6daf9df
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-03-19 13:48:02 +0800

    Update mdpy.analyser package, Update relative import to absolute import

--- mdpy/analyser/__init__.py
+++ mdpy/analyser/__init__.py
@@ -4,20 +4,20 @@ __email__ = "zhenyuwei99@gmail.com"
 __copyright__ = "Copyright 2021-2021, Southeast University and Zhenyu Wei"
 __license__ = "GPLv3"
 
-from .analyser_result import AnalyserResult, load_analyser_result
+from mdpy.analyser.analyser_result import AnalyserResult, load_analyser_result
 
 # Dynamics properties
-from .diffusion_analyser import DiffusionAnalyser
-from .mobility_analyser import MobilityAnalyser
-from .rmsd_analyer import RMSDAnalyser
+from mdpy.analyser.diffusion_analyser import DiffusionAnalyser
+from mdpy.analyser.mobility_analyser import MobilityAnalyser
+from mdpy.analyser.rmsd_analyer import RMSDAnalyser
 
 # Thermodynamics properties
-from .rdf_analyser import RDFAnalyser
-from .coordination_number_analyser import CoordinationNumberAnalyser
-from .residence_time_analyser import ResidenceTimeAnalyser
+from mdpy.analyser.rdf_analyser import RDFAnalyser
+from mdpy.analyser.coordination_number_analyser import CoordinationNumberAnalyser
+from mdpy.analyser.residence_time_analyser import ResidenceTimeAnalyser
 
 # Free energy
-from .wham_analyser import WHAMAnalyser
+from mdpy.analyser.wham_analyser import WHAMAnalyser
 
 __all__ = [
     'load_analyser_result',
