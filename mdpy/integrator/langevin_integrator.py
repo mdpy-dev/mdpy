13ce0ae528eb7d0a03d23b9f1037bb63a0050d9a
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-04-02 11:00:55 +0800

    Update mdpy.integrator package, Update header, Update license, Update relative import to absolute import

--- mdpy/integrator/langevin_integrator.py
+++ mdpy/integrator/langevin_integrator.py
@@ -4,9 +4,7 @@
 file : langevin_integrator.py
 created time : 2021/10/25
 author : Zhenyu Wei
-version : 1.0
-contact : zhenyuwei99@gmail.com
-copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
+copyright : (C)Copyright 2021-present, mdpy organization
 '''
 
 import numpy as np
