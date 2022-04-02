b10ba8074c38b64d8b331bd5ce0e0e3b0ba9d164
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-04-02 10:59:53 +0800

    Update mdpy.dumper package, Update header, Update license, Update relative import to absolute import

--- mdpy/dumper/log_dumper.py
+++ mdpy/dumper/log_dumper.py
@@ -4,9 +4,7 @@
 file : log_dumper.py
 created time : 2021/10/21
 author : Zhenyu Wei
-version : 1.0
-contact : zhenyuwei99@gmail.com
-copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
+copyright : (C)Copyright 2021-present, mdpy organization
 '''
 
 import numpy as np
