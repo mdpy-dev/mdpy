bf9b67de211bbf4f8d2c12fdc9e4b5be94cae39e
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-04-02 11:34:01 +0800

    Update mdpy.test package, Update header, Update license, Update relative import to absolute import

--- mdpy/test/test_hdf5_dumper.py
+++ mdpy/test/test_hdf5_dumper.py
@@ -4,9 +4,7 @@
 file : test_hdf5_dumper.py
 created time : 2022/02/25
 author : Zhenyu Wei
-version : 1.0
-contact : zhenyuwei99@gmail.com
-copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
+copyright : (C)Copyright 2021-present, mdpy organization
 '''
 
 import pytest, os
