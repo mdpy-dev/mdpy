c97dfed9ac9043e0dffdf705f46429a0018e63d9
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-04-02 11:53:13 +0800

    Update mdpy package, Update header, Update license, Update relative import to absolute import

--- mdpy/__init__.py
+++ mdpy/__init__.py
@@ -1,8 +1,7 @@
 __author__ = "Zhenyu Wei"
 __maintainer__ = "Zhenyu Wei"
-__email__ = "zhenyuwei99@gmail.com"
-__copyright__ = "Copyright 2021-2021, Southeast University and Zhenyu Wei"
-__license__ = "GPLv3"
+__copyright__ = "(C)Copyright 2021-present, mdpy organization"
+__license__ = "BSD"
 
 # Constant
 SPATIAL_DIM = 3
@@ -20,4 +19,4 @@ import mdpy.integrator as integrator
 import mdpy.minimizer as minimizer
 from mdpy.simulation import Simulation
 import mdpy.dumper as dumper
-import mdpy.analyser as analyser 
+import mdpy.analyser as analyser
