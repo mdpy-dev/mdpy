5518f44760cc59a3de4232efb36e302aed975cf4
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-04-02 11:00:26 +0800

    Update mdpy.forcefield package, Update header, Update license, Update relative import to absolute import

--- mdpy/forcefield/amber_forcefield.py
+++ mdpy/forcefield/amber_forcefield.py
@@ -4,8 +4,6 @@
 file : amber_forcefield.py
 created time : 2021/10/05
 author : Zhenyu Wei
-version : 1.0
-contact : zhenyuwei99@gmail.com
-copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
+copyright : (C)Copyright 2021-present, mdpy organization
 '''
 
