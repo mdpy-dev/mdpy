edf7b19749e597fc9e5eabc765e438aec4a7df1f
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-04-02 11:34:01 +0800

    Update mdpy.test package, Update header, Update license, Update relative import to absolute import

--- mdpy/test/test_charmm_nonbonded_constraint.py
+++ mdpy/test/test_charmm_nonbonded_constraint.py
@@ -4,9 +4,7 @@
 file : test_charmm_nonbonded_constraint.py
 created time : 2021/10/12
 author : Zhenyu Wei
-version : 1.0
-contact : zhenyuwei99@gmail.com
-copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
+copyright : (C)Copyright 2021-present, mdpy organization
 '''
 
 import pytest, os
