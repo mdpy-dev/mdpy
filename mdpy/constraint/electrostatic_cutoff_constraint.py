d2a69bc298d9899cbcdec7a400cb0d9eb00e3ad7
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-04-09 20:23:12 +0800

    Update mdpy.constraint.ElectrostaticCutoffConstraint class: remove cpu kernel

--- mdpy/constraint/electrostatic_cutoff_constraint.py
+++ mdpy/constraint/electrostatic_cutoff_constraint.py
@@ -18,24 +18,19 @@ from mdpy.constraint import Constraint, NUM_NEIGHBOR_CELLS, NEIGHBOR_CELL_TEMPLA
 from mdpy.utils import *
 from mdpy.unit import *
 
-epsilon0 = EPSILON0.value
-
 class ElectrostaticCutoffConstraint(Constraint):
     def __init__(self, parameters=None, force_id: int = 0, force_group: int = 0) -> None:
         super().__init__(parameters, force_id=force_id, force_group=force_group)
+        # Attributes
         self._int_parameters = []
         self._float_parameters = []
-        if env.platform == 'CPU':
-            self._kernel = nb.njit((
-                env.NUMBA_FLOAT[:, ::1], env.NUMBA_FLOAT[:, ::1], env.NUMBA_INT[:, ::1],
-                env.NUMBA_FLOAT[:, ::1], env.NUMBA_FLOAT[:, ::1]
-            ))(self.cpu_kernel)
-        elif env.platform == 'CUDA':
-            self._kernel = cuda.jit(nb.void(
-                env.NUMBA_FLOAT[:, ::1], env.NUMBA_FLOAT[:, ::1], env.NUMBA_FLOAT[::1], env.NUMBA_INT[:, ::1], env.NUMBA_FLOAT[:, ::1],
-                env.NUMBA_INT[:, ::1], env.NUMBA_INT[:, :, :, ::1], env.NUMBA_INT[::1], env.NUMBA_INT[:, ::1],
-                env.NUMBA_FLOAT[:, ::1], env.NUMBA_FLOAT[::1]
-            ))(self.cuda_kernel)
+        self._device_k = cuda.to_device(np.array([4 * np.pi * EPSILON0.value], dtype=env.NUMPY_FLOAT))
+        # Kernel
+        self._update = cuda.jit(nb.void(
+            env.NUMBA_FLOAT[:, ::1], env.NUMBA_FLOAT[:, ::1], env.NUMBA_FLOAT[::1], env.NUMBA_INT[:, ::1], env.NUMBA_FLOAT[:, ::1],
+            env.NUMBA_INT[:, ::1], env.NUMBA_INT[:, :, :, ::1], env.NUMBA_INT[::1], env.NUMBA_INT[:, ::1],
+            env.NUMBA_FLOAT[:, ::1], env.NUMBA_FLOAT[::1]
+        ))(self._update_kernel)
 
     def __repr__(self) -> str:
         return '<mdpy.constraint.ElectrostaticCutoffConstraint object>'
@@ -46,47 +41,19 @@ class ElectrostaticCutoffConstraint(Constraint):
     def bind_ensemble(self, ensemble: Ensemble):
         self._parent_ensemble = ensemble
         self._force_id = ensemble.constraints.index(self)
-        if env.platform == 'CUDA':
-            self._device_charges = cuda.to_device(self._parent_ensemble.topology.charges)
-            self._device_k = cuda.to_device(np.array([4 * np.pi * epsilon0], dtype=env.NUMPY_FLOAT))
+        self._device_charges = cuda.to_device(self._parent_ensemble.topology.charges)
+        self._device_pbc_matrix = cuda.to_device(self._parent_ensemble.state.pbc_matrix)
+        self._device_cutoff_radius = cuda.to_device(np.array([self._cutoff_radius], dtype=env.NUMPY_FLOAT))
+        self._device_bonded_particles = cuda.to_device(self._parent_ensemble.topology.bonded_particles)
+        self._device_scaling_particles = cuda.to_device(self._parent_ensemble.topology.scaling_particles)
 
     @staticmethod
-    def cpu_kernel(
-        positions, charges, bonded_particles,
-        pbc_matrix, pbc_inv
-    ):
-        forces = np.zeros_like(positions)
-        potential_energy = forces[0, 0]
-        num_particles = positions.shape[0]
-        k = 4 * np.pi * epsilon0
-        for id1 in range(num_particles):
-            cur_bonded_particles = bonded_particles[id1, :][bonded_particles[id1, :] != -1]
-            for id2 in range(id1+1, num_particles):
-                if not id2 in cur_bonded_particles:
-                    e1 = charges[id1, 0]
-                    e2 = charges[id2, 0]
-                    force_vec = unwrap_vec(
-                        positions[id2, :] - positions[id1, :],
-                        pbc_matrix, pbc_inv
-                    )
-                    dist = np.linalg.norm(force_vec)
-                    force_vec /= dist
-                    force_val = - e1 * e2 / k / dist**2
-                    force = force_vec * force_val
-                    forces[id1, :] += force
-                    forces[id2, :] -= force
-                    # Potential energy
-                    potential_energy += e1 * e2 / k / dist
-        return forces, potential_energy
-
-    @staticmethod
-    def cuda_kernel(
+    def _update_kernel(
         positions, charges, k, bonded_particles, pbc_matrix,
         particle_cell_index, cell_list, num_cell_vec, neighbor_cell_template,
         forces, potential_energy
     ):
-        thread_x = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
-        thread_y = cuda.blockIdx.y * cuda.blockDim.y + cuda.threadIdx.y
+        thread_x, thread_y = cuda.grid(2)
         num_particles_per_cell = cell_list.shape[3]
         num_particles = positions.shape[0]
         id1 = thread_x
@@ -136,40 +103,29 @@ class ElectrostaticCutoffConstraint(Constraint):
         cuda.atomic.add(potential_energy, 0, energy)
 
     def update(self):
-        self._check_bound_state()
-        if env.platform == 'CPU':
-            self._forces, self._potential_energy = self._kernel(
-                self._parent_ensemble.state.positions,
-                self._parent_ensemble.topology.charges,
-                self._parent_ensemble.topology.bonded_particles,
-                *self._parent_ensemble.state.pbc_info
-            )
-        elif env.platform == 'CUDA':
-            self._forces = np.zeros_like(self._parent_ensemble.state.positions)
-            self._potential_energy = np.zeros([1], dtype=env.NUMPY_FLOAT)
-            d_positions = cuda.to_device(self._parent_ensemble.state.positions)
-            d_bonded_particles = cuda.to_device(self._parent_ensemble.topology.bonded_particles)
-            d_pbc_matrix = cuda.to_device(self._parent_ensemble.state.pbc_matrix)
-            d_particle_cell_index = cuda.to_device(self._parent_ensemble.state.cell_list.particle_cell_index)
-            d_cell_list = cuda.to_device(self._parent_ensemble.state.cell_list.cell_list)
-            d_num_cell_vec = cuda.to_device(self._parent_ensemble.state.cell_list.num_cell_vec)
-            d_neighbor_cell_template = cuda.to_device(NEIGHBOR_CELL_TEMPLATE.astype(env.NUMPY_INT))
-            d_forces = cuda.to_device(self._forces)
-            d_potential_energy = cuda.to_device(self._potential_energy)
-            thread_per_block = (8, 8)
-            block_per_grid_x = int(np.ceil(
-                self._parent_ensemble.topology.num_particles / thread_per_block[0]
-            ))
-            block_per_grid_y = int(np.ceil(
-                self._parent_ensemble.state.cell_list.num_particles_per_cell * NUM_NEIGHBOR_CELLS / thread_per_block[1]
-            ))
-            block_per_grid = (block_per_grid_x, block_per_grid_y)
-            self._kernel[block_per_grid, thread_per_block](
-                d_positions, self._device_charges, self._device_k,
-                d_bonded_particles, d_pbc_matrix,
-                d_particle_cell_index, d_cell_list,
-                d_num_cell_vec, d_neighbor_cell_template,
-                d_forces, d_potential_energy
-            )
-            self._forces = d_forces.copy_to_host()
-            self._potential_energy = d_potential_energy.copy_to_host()[0]
+        self._forces = np.zeros_like(self._parent_ensemble.state.positions)
+        self._potential_energy = np.zeros([1], dtype=env.NUMPY_FLOAT)
+        device_positions = cuda.to_device(self._parent_ensemble.state.positions)
+        device_particle_cell_index = cuda.to_device(self._parent_ensemble.state.cell_list.particle_cell_index)
+        device_cell_list = cuda.to_device(self._parent_ensemble.state.cell_list.cell_list)
+        device_num_cell_vec = cuda.to_device(self._parent_ensemble.state.cell_list.num_cell_vec)
+        device_neighbor_cell_template = cuda.to_device(NEIGHBOR_CELL_TEMPLATE.astype(env.NUMPY_INT))
+        device_forces = cuda.to_device(self._forces)
+        device_potential_energy = cuda.to_device(self._potential_energy)
+        thread_per_block = (8, 8)
+        block_per_grid_x = int(np.ceil(
+            self._parent_ensemble.topology.num_particles / thread_per_block[0]
+        ))
+        block_per_grid_y = int(np.ceil(
+            self._parent_ensemble.state.cell_list.num_particles_per_cell * NUM_NEIGHBOR_CELLS / thread_per_block[1]
+        ))
+        block_per_grid = (block_per_grid_x, block_per_grid_y)
+        self._update[block_per_grid, thread_per_block](
+            device_positions, self._device_charges, self._device_k,
+            self._device_bonded_particles, self._device_pbc_matrix,
+            device_particle_cell_index, device_cell_list,
+            device_num_cell_vec, device_neighbor_cell_template,
+            device_forces, device_potential_energy
+        )
+        self._forces = device_forces.copy_to_host()
+        self._potential_energy = device_potential_energy.copy_to_host()[0]
