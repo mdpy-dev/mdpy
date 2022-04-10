15f6a0c15c5c1f7bec894bcee42c791a0334b0c2
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-04-10 12:20:25 +0800

    Update mdpy.constrant package, remove NUM_NEIGHBOR_CELLS, NEIGHBOR_CELL_TEMPLATE constant

--- mdpy/constraint/__init__.py
+++ mdpy/constraint/__init__.py
@@ -4,20 +4,7 @@ __copyright__ = "(C)Copyright 2021-present, mdpy organization"
 __license__ = "BSD"
 
 from .constraint import Constraint
-
-# Constants
-import numpy as np
-from mdpy import SPATIAL_DIM, env
 from mdpy.error import *
-
-NUM_NEIGHBOR_CELLS = 27
-NEIGHBOR_CELL_TEMPLATE = np.zeros([NUM_NEIGHBOR_CELLS, SPATIAL_DIM], dtype=env.NUMPY_INT)
-index = 0
-for i in range(-1, 2):
-    for j in range(-1, 2):
-        for k in range(-1, 2):
-            NEIGHBOR_CELL_TEMPLATE[index, :] = [i, j, k]
-            index += 1
 
 # Electrostatic
 LONG_RANGE_SOLVER = ['PME', 'CUTOFF']
