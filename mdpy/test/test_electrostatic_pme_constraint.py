899780aec83d3cc46cce2e3bef172b17cb90f865
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-04-10 19:29:54 +0800

    Update test for mdpy.constraint.ElectrostaticPMEConstraint._get_grid_size method

--- mdpy/test/test_electrostatic_pme_constraint.py
+++ mdpy/test/test_electrostatic_pme_constraint.py
@@ -69,3 +69,36 @@ class TestElectrostaticPMEConstraint:
         # Non-neutralized system
         with pytest.raises(EnsemblePoorDefinedError):
             self.ensemble.add_constraints(self.constraint)
+
+    def test_get_grid_size(self):
+        p1 = Particle(
+            particle_id=0, particle_type='C',
+            particle_name='CA', molecule_type='ASN',
+            mass=12, charge=1
+        )
+        p2 = Particle(
+            particle_id=1, particle_type='N',
+            particle_name='NY', molecule_type='ASN',
+            mass=14, charge=-1
+        )
+        p3 = Particle(
+            particle_id=2, particle_type='CA',
+            particle_name='CPT', molecule_type='ASN',
+            mass=1, charge=0
+        )
+        p4 = Particle(
+            particle_id=3, particle_type='C',
+            particle_name='CA', molecule_type='ASN',
+            mass=12, charge=0
+        )
+        t = Topology()
+        t.add_particles([p1, p2, p3, p4])
+        p = np.array([
+            [0, 0, 0], [0, 10, 0], [0, 21, 0], [0, 11, 0]
+        ])
+        ensemble = Ensemble(t, np.eye(3)*30)
+        ensemble.add_constraints(self.constraint)
+        assert self.constraint._get_grid_size()[0] == 32
+
+        ensemble.state.set_pbc_matrix(np.eye(3)*65)
+        assert self.constraint._get_grid_size()[1] == 96
