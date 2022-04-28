50b41d757f6d5238b6af1a7b4772562ba78862fc
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-04-28 13:24:05 +0800

    Update mdpy.constraint.CharmmAngleConstraint: Add _block_per_grid attribute

--- mdpy/constraint/charmm_angle_constraint.py
+++ mdpy/constraint/charmm_angle_constraint.py
@@ -65,6 +65,9 @@ class CharmmAngleConstraint(Constraint):
             self._num_angles += 1
         self._int_parameters = cp.array(np.vstack(self._int_parameters), CUPY_INT)
         self._float_parameters = cp.array(np.vstack(self._float_parameters), CUPY_FLOAT)
+        self._block_per_grid = (int(np.ceil(
+            self._parent_ensemble.topology.num_angles / THREAD_PER_BLOCK
+        )))
 
     @staticmethod
     def _cpu_kernel(int_parameters, float_parameters, positions, pbc_matrix, pbc_inv):
@@ -292,10 +295,7 @@ class CharmmAngleConstraint(Constraint):
         self._forces = cp.zeros_like(self._parent_ensemble.state.positions, CUPY_FLOAT)
         self._potential_energy = cp.zeros([1], CUPY_FLOAT)
         # Device
-        block_per_grid = (int(np.ceil(
-            self._parent_ensemble.topology.num_angles / THREAD_PER_BLOCK
-        )))
-        self._update[block_per_grid, THREAD_PER_BLOCK](
+        self._update[self._block_per_grid, THREAD_PER_BLOCK](
             self._int_parameters,
             self._float_parameters,
             self._parent_ensemble.state.device_positions,
