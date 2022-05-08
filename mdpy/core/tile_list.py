579f84e8116d6eae576ce79e4bfa77a3ae0dd672
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-05-08 12:51:24 +0800

    Update mdpy.core.TileList class: Add sort_matrix and unsort_matrix method

--- mdpy/core/tile_list.py
+++ mdpy/core/tile_list.py
@@ -45,6 +45,7 @@ class TileList:
             NUMBA_INT[:, ::1], # cell_particle_information
             NUMBA_INT[:, ::1], # cell_tile_information
             NUMBA_INT[::1], # num_cells_vec
+            NUMBA_INT[::1], # sorted_matrix_index
             NUMBA_INT[:, ::1], # tile_list
             NUMBA_INT[:, ::1] # tile_cell_index
         ))(self._update_tile_list_kernel)
@@ -55,6 +56,26 @@ class TileList:
             NUMBA_INT[::1], # num_cells_vec
             NUMBA_INT[:, ::1], # tile_neighbor
         ))(self._update_tile_neighbor_kernel)
+        self._sort_int_matrix = cuda.jit(nb.void(
+            NUMBA_INT[:, ::1], # unsorted_matrix
+            NUMBA_INT[::1], # sorted_matrix_mapping_index
+            NUMBA_INT[:, ::1] # sorted_matrix
+        ))(self._sort_matrix_kernel)
+        self._sort_float_matrix = cuda.jit(nb.void(
+            NUMBA_FLOAT[:, ::1], # unsorted_matrix
+            NUMBA_INT[::1], # sorted_matrix_mapping_index
+            NUMBA_FLOAT[:, ::1] # sorted_matrix
+        ))(self._sort_matrix_kernel)
+        self._unsort_int_matrix = cuda.jit(nb.void(
+            NUMBA_INT[:, ::1], # sorted_matrix
+            NUMBA_INT[::1], # sorted_matrix_mapping_index
+            NUMBA_INT[:, ::1] # unsorted_matrix
+        ))(self._unsort_matrix_kernel)
+        self._unsort_float_matrix = cuda.jit(nb.void(
+            NUMBA_FLOAT[:, ::1], # sorted_matrix
+            NUMBA_INT[::1], # sorted_matrix_mapping_index
+            NUMBA_FLOAT[:, ::1] # unsorted_matrix
+        ))(self._unsort_matrix_kernel)
 
     def set_pbc_matrix(self, pbc_matrix: np.ndarray):
         pbc_matrix = check_quantity_value(pbc_matrix, default_length_unit)
@@ -134,8 +155,9 @@ class TileList:
         cell_particle_information,
         cell_tile_information,
         num_cells_vec,
+        sorted_matrix_mapping_index,
         tile_list,
-        tile_cell_index
+        tile_cell_index,
     ):
         tile_index = cuda.grid(1)
         num_tiles = tile_list.shape[0]
@@ -174,13 +196,13 @@ class TileList:
             tile_particle_end_index = cell_particle_end_index
         # Assign particles to tile
         tile_particle_index = 0
-
-        dif = tile_particle_end_index - tile_particle_start_index
-        if dif <= 0:
-            print(cell_tile_start_index, cell_tile_information[cell_index, 1], tile_index)
-        for particle_index in range(tile_particle_start_index, tile_particle_end_index):
-            tile_list[tile_index, tile_particle_index] = sorted_particle_index[particle_index]
+        sorted_matrix_index = tile_index * NUM_PARTICLES_PER_TILE # The index of sorted_particle
+        for index in range(tile_particle_start_index, tile_particle_end_index):
+            particle_index = sorted_particle_index[index]
+            tile_list[tile_index, tile_particle_index] = particle_index
+            sorted_matrix_mapping_index[sorted_matrix_index] = particle_index
             tile_particle_index += 1
+            sorted_matrix_index += 1
         # Get 3d cell index
         decomposition = cell_index
         num_cells_z = num_cells_vec[2]
@@ -241,7 +263,70 @@ class TileList:
                         cur_neighbor_tile_index += 1
                         cur_tile_index += 1
 
+    def sort_matrix(self, unsorted_matrix: cp.ndarray):
+        matrix_type = unsorted_matrix.dtype
+        sorted_matrix = cp.zeros(
+            (self._num_tiles*NUM_PARTICLES_PER_TILE, unsorted_matrix.shape[1]),
+            dtype=matrix_type
+        )
+        thread_per_block = 32
+        block_per_grid = self._num_tiles
+        if matrix_type == CUPY_INT:
+            self._sort_int_matrix[block_per_grid, thread_per_block](
+                unsorted_matrix,
+                self._sorted_matrix_mapping_index,
+                sorted_matrix
+            )
+        elif matrix_type == CUPY_FLOAT:
+            self._sort_float_matrix[block_per_grid, thread_per_block](
+                unsorted_matrix,
+                self._sorted_matrix_mapping_index,
+                sorted_matrix
+            )
+        return sorted_matrix
+
+    @staticmethod
+    def _sort_matrix_kernel(unsorted_matrix, sorted_matrix_mapping_index, sorted_matrix):
+        idx = cuda.grid(1)
+        unsorted_index = sorted_matrix_mapping_index[idx]
+        if unsorted_index == -1:
+            return
+        for i in range(unsorted_matrix.shape[1]):
+            sorted_matrix[idx, i] = unsorted_matrix[unsorted_index, i]
+
+    def unsort_matrix(self, sorted_matrix: cp.ndarray):
+        matrix_type = sorted_matrix.dtype
+        unsorted_matrix = cp.zeros(
+            (self._num_particles, sorted_matrix.shape[1]),
+            dtype=matrix_type
+        )
+        thread_per_block = 32
+        block_per_grid = self._num_tiles
+        if matrix_type == CUPY_INT:
+            self._unsort_int_matrix[block_per_grid, thread_per_block](
+                sorted_matrix,
+                self._sorted_matrix_mapping_index,
+                unsorted_matrix
+            )
+        elif matrix_type == CUPY_FLOAT:
+            self._unsort_float_matrix[block_per_grid, thread_per_block](
+                sorted_matrix,
+                self._sorted_matrix_mapping_index,
+                unsorted_matrix
+            )
+        return unsorted_matrix
+
+    @staticmethod
+    def _unsort_matrix_kernel(sorted_matrix, sorted_matrix_mapping_index, unsorted_matrix):
+        idx = cuda.grid(1)
+        unsorted_index = sorted_matrix_mapping_index[idx]
+        if unsorted_index == -1:
+            return
+        for i in range(unsorted_matrix.shape[1]):
+            unsorted_matrix[unsorted_index, i] = sorted_matrix[idx, i]
+
     def update(self, positions: cp.ndarray):
+        self._num_particles = positions.shape[0]
         positive_postions = positions + self._device_half_pbc_diag
         result = self._update_cell_information_kernel(
             positive_postions, self._num_cells_vec, self._device_cell_width
@@ -255,11 +340,13 @@ class TileList:
         block_per_grid = int(np.ceil(self._num_tiles / THREAD_PER_BLOCK))
         self._tile_list = cp.zeros((self._num_tiles, NUM_PARTICLES_PER_TILE), dtype=CUPY_INT) - 1
         self._tile_cell_index = cp.zeros((self._num_tiles, SPATIAL_DIM), dtype=CUPY_INT)
+        self._sorted_matrix_mapping_index = cp.zeros(self._num_tiles*NUM_PARTICLES_PER_TILE, dtype=CUPY_INT) - 1
         self._update_tile_list[block_per_grid, THREAD_PER_BLOCK](
             sorted_particle_index,
             cell_particle_information,
             cell_tile_information,
             self._device_num_cells_vec,
+            self._sorted_matrix_mapping_index,
             self._tile_list,
             self._tile_cell_index
         )
@@ -297,13 +384,60 @@ class TileList:
         return self._tile_neighbors
 
     @property
-    def tile_cell_index(self) -> cp.ndarray:
-        return self._tile_cell_index
-
-    @property
     def num_cells_vec(self):
         return self._num_cells_vec
 
     @property
     def device_num_cells_vec(self):
         return self._device_num_cells_vec
+
+if __name__ == '__main__':
+    import time
+    import mdpy as md
+    from cupy.cuda.nvtx import RangePush, RangePop
+    pdb = md.io.PDBParser('/home/zhenyuwei/nutstore/ZhenyuWei/Note_Research/mdpy/mdpy/benchmark/str/medium.pdb')
+    positions = cp.array(pdb.positions, CUPY_FLOAT)
+    positive_positions = positions + cp.array(np.diagonal(pdb.pbc_matrix)) / 2
+
+    tile_list = TileList(pdb.pbc_matrix)
+    tile_list.set_cutoff_radius(8)
+
+    tile_list.update(positions)
+
+    print(cp.count_nonzero(tile_list.tile_list+1))
+    if not True: # sort_matrix
+        for tile in range(32):
+            print('# Tile %d, cell_index: %s' %(tile, tile_list.tile_cell_index[tile, :]))
+            print(tile_list.sorted_matrix_mapping_index[tile*NUM_PARTICLES_PER_TILE:(tile+1)*NUM_PARTICLES_PER_TILE])
+            print(tile_list.sort_matrix(positive_positions)[tile*NUM_PARTICLES_PER_TILE:(tile+1)*NUM_PARTICLES_PER_TILE, :])
+    if True:
+        sorted_positions = tile_list.sort_matrix(positions)
+        unsorted_positions = tile_list.unsort_matrix(sorted_positions)
+        print(cp.hstack([positions, unsorted_positions])[:100, :])
+
+    epoch = 30
+    RangePush('Update tile')
+    ts = time.time()
+    for i in range(epoch):
+        tile_list.update(positions)
+    te = time.time()
+    print('Run update for %s s' %((te-ts)/epoch))
+    RangePop()
+
+    RangePush('Sort matrix')
+    ts = time.time()
+    for i in range(epoch):
+        tile_list.sort_matrix(positions)
+    te = time.time()
+    print('Run sort for %s s' %((te-ts)/epoch))
+    RangePop()
+
+    sorted_positions = tile_list.sort_matrix(positions)
+    RangePush('Unsort matrix')
+    ts = time.time()
+    for i in range(epoch):
+        tile_list.unsort_matrix(sorted_positions)
+    te = time.time()
+    print('Run unsort for %s s' %((te-ts)/epoch))
+    RangePop()
+    
