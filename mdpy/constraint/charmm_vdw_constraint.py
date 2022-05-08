382c4bc0824c3b5a327215513838185d7c466cca
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-05-08 22:23:36 +0800

    Update mdpy.constraint.CharmmVDWConstraint class: Try for sorted_matrix

--- mdpy/constraint/charmm_vdw_constraint.py
+++ mdpy/constraint/charmm_vdw_constraint.py
@@ -20,7 +20,7 @@ from mdpy.constraint import Constraint
 from mdpy.utils import *
 from mdpy.unit import *
 
-THREAD_PER_BLOCK = (32, 1)
+THREAD_PER_BLOCK = (NUM_PARTICLES_PER_TILE, 1)
 class CharmmVDWConstraint(Constraint):
     def __init__(self, parameter_dict: dict, cutoff_radius=Quantity(12, angstrom)) -> None:
         super().__init__()
@@ -28,18 +28,18 @@ class CharmmVDWConstraint(Constraint):
         self._parameter_dict = parameter_dict
         self._cutoff_radius = check_quantity_value(cutoff_radius, default_length_unit)
         self._device_cutoff_radius = cp.array([self._cutoff_radius], CUPY_FLOAT)
-        self._parameters_list = []
+        self._parameters = []
         # Kernel
         self._update = cuda.jit(nb.void(
-            NUMBA_FLOAT[:, ::1], # positions
-            NUMBA_FLOAT[:, ::1], # parameters
             NUMBA_FLOAT[::1], # cutoff_radius
             NUMBA_FLOAT[:, ::1], # pbc_matrix
-            NUMBA_INT[:, ::1], # excluded_particles
-            NUMBA_INT[:, ::1], # scaled_particles
-            NUMBA_INT[:, ::1], # tile_list
+            NUMBA_FLOAT[:, ::1], # sorted_positions
+            NUMBA_FLOAT[:, ::1], # sorted_parameters
+            NUMBA_INT[:, ::1], # sorted_excluded_particles
+            NUMBA_INT[:, ::1], # sorted_scaled_particles
+            NUMBA_INT[::1], # sorted_matrix_mapping_index
             NUMBA_INT[:, ::1], # tile_neighbors
-            NUMBA_FLOAT[:, ::1], # forces
+            NUMBA_FLOAT[:, ::1], # sorted_forces
             NUMBA_FLOAT[::1] # potential_energy
         ))(self._update_charmm_vdw_kernel)
 
@@ -52,87 +52,89 @@ class CharmmVDWConstraint(Constraint):
     def bind_ensemble(self, ensemble: Ensemble):
         self._parent_ensemble = ensemble
         self._constraint_id = ensemble.constraints.index(self)
-        self._parameters_list = []
+        self._parameters = []
         for particle in self._parent_ensemble.topology.particles:
             param = self._parameter_dict[particle.particle_type]
             if len(param) == 2:
                 epsilon, sigma = param
-                self._parameters_list.append([epsilon, sigma, epsilon, sigma])
+                self._parameters.append([epsilon, sigma, epsilon, sigma])
             elif len(param) == 4:
                 epsilon, sigma, epsilon14, sigma14 = param
-                self._parameters_list.append([epsilon, sigma, epsilon14, sigma14])
-        self._device_parameters_list = cp.array(np.vstack(self._parameters_list), CUPY_FLOAT)
+                self._parameters.append([epsilon, sigma, epsilon14, sigma14])
+        self._device_parameters = cp.array(np.vstack(self._parameters), CUPY_FLOAT)
 
     @staticmethod
     def _update_charmm_vdw_kernel(
-        positions,
-        parameters,
         cutoff_radius,
         pbc_matrix,
-        excluded_particles,
-        scaled_particles,
-        tile_list,
+        sorted_positions,
+        sorted_parameters,
+        sorted_excluded_particles,
+        sorted_scaled_particles,
+        sorted_matrix_mapping_index,
         tile_neighbors,
-        forces, potential_energy
+        sorted_forces, potential_energy
     ):
         # tile information
         tile_id1 = cuda.blockIdx.x
         tile_id2 = tile_neighbors[tile_id1, cuda.blockIdx.y]
-        thread_x = cuda.threadIdx.x
-        if tile_id1 >= tile_list.shape[0]:
-            return
         if tile_id2 == -1:
             return
+        # Particle index information
+        local_thread_x = cuda.threadIdx.x
+        global_thread_x = tile_id1 * cuda.blockDim.x + local_thread_x
+        tile2_start_index = tile_id2 * NUM_PARTICLES_PER_TILE
         # shared data
-        shared_particle_index = cuda.shared.array(shape=(NUM_PARTICLES_PER_TILE), dtype=NUMBA_INT)
-        shared_positions = cuda.shared.array(shape=(NUM_PARTICLES_PER_TILE, SPATIAL_DIM), dtype=NUMBA_FLOAT)
-        shared_parameters = cuda.shared.array(shape=(NUM_PARTICLES_PER_TILE, 4), dtype=NUMBA_FLOAT)
         shared_pbc_matrix = cuda.shared.array(shape=(SPATIAL_DIM), dtype=NUMBA_FLOAT)
         shared_half_pbc_matrix = cuda.shared.array(shape=(SPATIAL_DIM), dtype=NUMBA_FLOAT)
-        if thread_x == 0:
+        if local_thread_x <= 2:
+            shared_pbc_matrix[local_thread_x] = pbc_matrix[local_thread_x, local_thread_x]
+            shared_half_pbc_matrix[local_thread_x] = shared_pbc_matrix[local_thread_x] / 2
+        shared_positions = cuda.shared.array(shape=(NUM_PARTICLES_PER_TILE, SPATIAL_DIM), dtype=NUMBA_FLOAT)
+        shared_parameters = cuda.shared.array(shape=(NUM_PARTICLES_PER_TILE, 4), dtype=NUMBA_FLOAT)
+        shared_particle_index_2 = cuda.shared.array(shape=(NUM_PARTICLES_PER_TILE), dtype=NUMBA_INT)
+        index = tile2_start_index+local_thread_x
+        shared_particle_index_2[local_thread_x] = sorted_matrix_mapping_index[index]
+        if shared_particle_index_2[local_thread_x] != -1:
             for i in range(SPATIAL_DIM):
-                shared_pbc_matrix[i] = pbc_matrix[i, i]
-                shared_half_pbc_matrix[i] = shared_pbc_matrix[i] / 2
-        shared_particle_index[thread_x] = tile_list[tile_id2, thread_x]
-        for i in range(SPATIAL_DIM):
-            shared_positions[thread_x, i] = positions[shared_particle_index[thread_x], i]
-        for i in range(4):
-            shared_parameters[thread_x, i] = parameters[shared_particle_index[thread_x], i]
+                shared_positions[local_thread_x, i] = sorted_positions[i, index]
+            for i in range(4):
+                shared_parameters[local_thread_x, i] = sorted_parameters[i, index]
         cuda.syncthreads()
 
         # Local data
-        particle_1 = tile_list[tile_id1, thread_x]
+        particle_1 = sorted_matrix_mapping_index[global_thread_x]
         if particle_1 == -1:
             return
         local_excluded_particles = cuda.local.array(shape=(MAX_NUM_EXCLUDED_PARTICLES), dtype=NUMBA_INT)
         num_excluded_particles = 0
         for i in range(MAX_NUM_EXCLUDED_PARTICLES):
-            local_excluded_particles[i] = excluded_particles[particle_1, i]
+            local_excluded_particles[i] = sorted_excluded_particles[i, global_thread_x]
             if local_excluded_particles[i] == -1:
                 break
             num_excluded_particles += 1
         local_scaled_particles = cuda.local.array(shape=(MAX_NUM_SCALED_PARTICLES), dtype=NUMBA_INT)
         num_scaled_particles = 0
         for i in range(MAX_NUM_SCALED_PARTICLES):
-            local_scaled_particles[i] = scaled_particles[particle_1, i]
+            local_scaled_particles[i] = sorted_scaled_particles[i, global_thread_x]
             if local_scaled_particles[i] == -1:
                 break
             num_scaled_particles += 1
         local_parameters = cuda.local.array(shape=(SPATIAL_DIM), dtype=NUMBA_FLOAT)
         for i in range(4):
-            local_parameters[i] = parameters[particle_1, i]
+            local_parameters[i] = sorted_parameters[i, global_thread_x]
         local_positions = cuda.local.array(shape=(SPATIAL_DIM), dtype=NUMBA_FLOAT)
         local_forces = cuda.local.array(shape=(SPATIAL_DIM), dtype=NUMBA_FLOAT)
         vec = cuda.local.array(shape=(SPATIAL_DIM), dtype=NUMBA_FLOAT)
         energy = 0
         cutoff_radius = cutoff_radius[0]
         for i in range(SPATIAL_DIM):
-            local_positions[i] = positions[particle_1, i]
+            local_positions[i] = sorted_positions[i, global_thread_x]
             local_forces[i] = 0
 
         # Computation
         for index in range(NUM_PARTICLES_PER_TILE):
-            particle_2 = shared_particle_index[index]
+            particle_2 = shared_particle_index_2[index]
             if particle_2 == -1:
                 break
             if particle_1 == particle_2:
@@ -180,32 +182,32 @@ class CharmmVDWConstraint(Constraint):
                 for i in range(SPATIAL_DIM):
                     local_forces[i] += force_val * vec[i]
         for i in range(SPATIAL_DIM):
-            cuda.atomic.add(forces, (particle_1, i), local_forces[i])
+            sorted_forces[i, global_thread_x] += local_forces[i]
         cuda.atomic.add(potential_energy, 0, energy)
 
     def update(self):
         self._check_bound_state()
-        self._forces = cp.zeros(self._parent_ensemble.state.matrix_shape, CUPY_FLOAT)
+        sorted_forces = cp.zeros((SPATIAL_DIM, self._parent_ensemble.tile_list.num_tiles * NUM_PARTICLES_PER_TILE), CUPY_FLOAT)
         self._potential_energy = cp.zeros([1], CUPY_FLOAT)
-        block_per_grid_x = int(np.ceil(
-            self._parent_ensemble.tile_list.num_tiles * NUM_PARTICLES_PER_TILE / THREAD_PER_BLOCK[0]
-        ))
-        block_per_grid_y = int(np.ceil(
-            self._parent_ensemble.tile_list.tile_neighbors.shape[1]
-        ))
-        self._block_per_grid = (block_per_grid_x, block_per_grid_y)
+        sorted_positions = self._parent_ensemble.tile_list.sort_matrix(self._parent_ensemble.state.positions)
+        print(sorted_positions.flags)
+        device_sorted_parameter_list = self._parent_ensemble.tile_list.sort_matrix(self._device_parameters)
+        block_per_grid_x = self._parent_ensemble.tile_list.num_tiles
+        block_per_grid_y = self._parent_ensemble.tile_list.tile_neighbors.shape[1]
+        block_per_grid = (block_per_grid_x, block_per_grid_y)
         # Device
-        self._update[self._block_per_grid, THREAD_PER_BLOCK](
-            self._parent_ensemble.state.positions,
-            self._device_parameters_list,
+        self._update[block_per_grid, THREAD_PER_BLOCK](
             self._device_cutoff_radius,
             self._parent_ensemble.state.device_pbc_matrix,
-            self._parent_ensemble.topology.device_excluded_particles,
-            self._parent_ensemble.topology.device_scaled_particles,
-            self._parent_ensemble.tile_list.tile_list,
+            sorted_positions,
+            device_sorted_parameter_list,
+            self._parent_ensemble.topology.device_sorted_excluded_particles,
+            self._parent_ensemble.topology.device_sorted_scaled_particles,
+            self._parent_ensemble.tile_list.sorted_matrix_mapping_index,
             self._parent_ensemble.tile_list.tile_neighbors,
-            self._forces, self._potential_energy
+            sorted_forces, self._potential_energy
         )
+        self._forces = self._parent_ensemble.tile_list.unsort_matrix(sorted_forces)
 
 if __name__ == '__main__':
     import os
@@ -228,8 +230,7 @@ if __name__ == '__main__':
     ensemble.add_constraints(constraint)
     id1, id2 = 6570, 6569
     ensemble.state.set_positions(pdb.positions)
-    ensemble.tile_list.update(ensemble.state.positions)
+    ensemble.update_tile_list()
     print(ensemble.tile_list.tile_neighbors.shape)
     constraint.update()
-    # print(constraint.forces)
     print(Quantity(constraint.forces, default_force_unit).convert_to(kilojoule_permol_over_nanometer).value)
