6ba985f68c969b34583bf4054652ddf580fc62ba
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-05-09 15:24:46 +0800

    Update test for mdpy.core.TileList class

--- mdpy/test/test_tile_list.py
+++ mdpy/test/test_tile_list.py
@@ -29,17 +29,17 @@ class TestTileList:
         self.tile_list = None
 
     def test_attributes(self):
-        assert self.tile_list.cutoff_radius == 3
-        assert self.tile_list.pbc_matrix[0, 0] == 30
+        assert self.tile_list.cutoff_radius == 8
+        assert self.tile_list.pbc_matrix[0, 0] == 80
 
     def test_exceptions(self):
         with pytest.raises(UnitDimensionDismatchedError):
             self.tile_list.set_cutoff_radius(Quantity(1, second))
 
-        with pytest.raises(NeighborListPoorDefinedError):
+        with pytest.raises(TileListPoorDefinedError):
             self.tile_list.set_cutoff_radius(0)
 
-        with pytest.raises(NeighborListPoorDefinedError):
+        with pytest.raises(TileListPoorDefinedError):
             self.tile_list.set_cutoff_radius(56)
 
     def test_update(self):
@@ -54,7 +54,7 @@ class TestTileList:
 
         # All particles has been assigned to the right tile
         for tile_index in range(0, self.tile_list.num_tiles, 100):
-            cell_index = self.tile_list.tile_cell_index[tile_index, :]
+            cell_index = self.tile_list._tile_cell_index[tile_index, :]
             position_lower = cell_index * self.tile_list._cell_width
             position_upper = (cell_index + 1) * self.tile_list._cell_width
             for particle_tile_index in range(NUM_PARTICLES_PER_TILE):
@@ -69,11 +69,11 @@ class TestTileList:
         # All neighbors has been assigned
         for tile_index in range(0, self.tile_list.num_tiles, 500):
             neighbors = self.tile_list.tile_neighbors[tile_index, :]
-            central_cell_index = self.tile_list.tile_cell_index[tile_index, :]
+            central_cell_index = self.tile_list._tile_cell_index[tile_index, :]
             for neighhor in neighbors[::10]:
                 if neighhor == -1:
                     break
-                neighbor_cell_index = self.tile_list.tile_cell_index[neighhor, :]
+                neighbor_cell_index = self.tile_list._tile_cell_index[neighhor, :]
                 for i in range(SPATIAL_DIM):
                     diff = neighbor_cell_index[i] - central_cell_index[i]
                     if diff > self.tile_list.num_cells_vec[i] / 2:
