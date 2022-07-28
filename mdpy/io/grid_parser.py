ea3c9862e8a473b2bb760240634c73bcafcb7195
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-07-28 20:24:45 +0800

    Add mdpy.io.GridParser and mdpy.io.GridWriter classes

---
+++ mdpy/io/grid_parser.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+file : grid_parser.py
+created time : 2022/07/28
+author : Zhenyu Wei
+copyright : (C)Copyright 2021-present, mdpy organization
+"""
+
+import h5py
+import ast
+import cupy as cp
+from mdpy.core import Grid
+from mdpy.environment import *
+from mdpy.error import *
+
+
+class GridParser:
+    def __init__(self, file_path: str) -> None:
+        if not file_path.endswith(".grid"):
+            raise FileFormatError("The file should end with .hdf5 suffix")
+        self._file_path = file_path
+        self._grid = self._parse_grid()
+
+    def _parse_grid(self) -> Grid:
+        grid = Grid()
+        with h5py.File(self._file_path, "r") as h5f:
+            # Information
+            grid.set_requirement(
+                ast.literal_eval(bytes.decode(h5f["information/requirement"][()]))
+            )
+            grid._num_dimensions = h5f["information/num_dimensions"][()]
+            grid._shape = list(h5f["information/shape"][()])
+            grid._inner_shape = list(h5f["information/inner_shape"][()])
+            grid._grid_width = h5f["information/grid_width"][()].astype(NUMPY_FLOAT)
+            grid._device_grid_width = cp.array(grid.grid_width, CUPY_FLOAT)
+            # Coordinate
+            self._parse_attribute(h5f, grid, "coordinate")
+            # Field
+            self._parse_attribute(h5f, grid, "field")
+            grid.check_requirement()
+            # Gradient
+            self._parse_attribute(h5f, grid, "gradient")
+            # Curvature
+            self._parse_attribute(h5f, grid, "curvature")
+            grid.check_requirement()
+        return grid
+
+    def _parse_attribute(self, handle: h5py.File, grid: Grid, attribute: str):
+        sub_grid = getattr(grid, attribute)
+        for key in handle[attribute].keys():
+            setattr(
+                sub_grid,
+                key,
+                cp.array(handle["%s/%s" % (attribute, key)][()], CUPY_FLOAT),
+            )
+
+    @property
+    def grid(self) -> Grid:
+        return self._grid
---
+++ mdpy/io/grid_writer.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+file : grid_writer.py
+created time : 2022/07/28
+author : Zhenyu Wei
+copyright : (C)Copyright 2021-present, mdpy organization
+"""
+
+import h5py
+from mdpy.core import Grid
+from mdpy.error import *
+
+
+class GridWriter:
+    def __init__(self, file_path: str, mode: str = "w") -> None:
+        if not file_path.endswith(".grid"):
+            raise FileFormatError("The file should end with .hdf5 suffix")
+        self._file_path = file_path
+        self._mode = mode
+        with h5py.File(self._file_path, self._mode) as f:
+            f.create_group("information")
+            f.create_group("coordinate")
+            f.create_group("field")
+            f.create_group("gradient")
+            f.create_group("curvature")
+
+    def write(self, grid: Grid):
+        grid.check_requirement()
+        self._write_information(grid)
+        self._write_attribute(grid, "coordinate")
+        self._write_attribute(grid, "field")
+        self._write_attribute(grid, "gradient")
+        self._write_attribute(grid, "curvature")
+
+    def _write_information(self, grid: Grid):
+        with h5py.File(self._file_path, "a") as h5f:
+            del h5f["information"]
+            h5f.create_group("information")
+            h5f["information/requirement"] = str(grid.requirement)
+            h5f["information/num_dimensions"] = grid.num_dimensions
+            h5f["information/shape"] = grid.shape
+            h5f["information/inner_shape"] = grid.inner_shape
+            h5f["information/grid_width"] = grid.grid_width
+
+    def _write_attribute(self, grid: Grid, attribute: str):
+        with h5py.File(self._file_path, "a") as h5f:
+            del h5f[attribute]
+            h5f.create_group(attribute)
+            sub_grid = getattr(grid, attribute)
+            for key in sub_grid.__dict__:
+                if not "SubGrid" in key:
+                    h5f["%s/%s" % (attribute, key)] = getattr(sub_grid, key).get()
