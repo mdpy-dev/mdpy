35a0efe7971d4db1200272a6a2d45e4ffe44a851
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-11-13 13:19:05 +0800

    Update mdpy.core.Grid class: 1. Set grid having uniform grid width; 2. Add constant attribute; 3. Remove curvature and gradient attribute

--- mdpy/core/grid.py
+++ mdpy/core/grid.py
@@ -28,143 +28,82 @@ class SubGrid:
 
 
 class Grid:
-    def __init__(self, **coordinate) -> None:
+    def __init__(self, grid_width: float, **coordinate_range) -> None:
+        # Input
+        self._grid_width = NUMPY_FLOAT(grid_width)
+        self._device_grid_width = CUPY_FLOAT(grid_width)
         # Initialize attributes
         self._coordinate = SubGrid("coordinate")
         self._field = SubGrid("field")
-        self._gradient = SubGrid("gradient")
-        self._curvature = SubGrid("curvature")
+        self._constant = SubGrid("constant")
         # Set grid information and coordinate
-        keys = list(coordinate.keys())
-        self._num_dimensions = len(keys)
-        self._shape = [value[2] for value in coordinate.values()]
-        self._inner_shape = [value[2] - 2 for value in coordinate.values()]
+        keys = list(coordinate_range.keys())
         grid = [
-            cp.linspace(start=value[0], stop=value[1], num=value[2], endpoint=True)
-            for value in coordinate.values()
+            cp.arange(start=value[0], stop=value[1] + grid_width, step=grid_width)
+            for value in coordinate_range.values()
         ]
-        self._device_grid_width = cp.array([i[1] - i[0] for i in grid], NUMPY_FLOAT)
-        self._grid_width = self._device_grid_width.get()
         grid = cp.meshgrid(*grid, indexing="ij")
+        self._shape = list(grid[0].shape)
+        self._inner_shape = [i - 2 for i in self._shape]
         for index, key in enumerate(keys):
             setattr(
-                self._coordinate, key, grid[index],
+                self._coordinate,
+                key,
+                grid[index],
             )
+        self._num_dimensions = len(keys)
         # Initialize requirement
-        self._requirement = {}
+        self._requirement = {"field": [], "constant": []}
 
-    def set_requirement(self, requirement: dict):
-        for key, value in requirement.items():
-            self._requirement[key] = {
-                "require_gradient": value["require_gradient"],
-                "require_curvature": value["require_curvature"],
-            }
-            if hasattr(self._field, key):
-                if self._requirement[key]["require_gradient"]:
-                    setattr(
-                        self._gradient,
-                        key,
-                        self.get_gradient(getattr(self._field, key)),
-                    )
-                if self._requirement[key]["require_curvature"]:
-                    setattr(
-                        self._curvature,
-                        key,
-                        self.get_curvature(getattr(self._field, key)),
-                    )
+    def set_requirement(
+        self, field_name_list: list[str], constant_name_list: list[str]
+    ):
+        self._requirement["field"] = field_name_list
+        self._requirement["constant"] = constant_name_list
 
     def check_requirement(self):
         is_all_set = True
-        for key, value in self._requirement.items():
+        exception = "Gird is not all set:\n"
+        for key in self._requirement["constant"]:
+            is_all_set &= hasattr(self._constant, key)
+            exception += "- grid.constant.%s: %s;\n" % (
+                key,
+                hasattr(self._constant, key),
+            )
+        for key in self._requirement["field"]:
             is_all_set &= hasattr(self._field, key)
-            if value["require_gradient"]:
-                is_all_set &= hasattr(self._gradient, key)
-            if value["require_curvature"]:
-                is_all_set &= hasattr(self._curvature, key)
+            exception += "- grid.field.%s: %s;\n" % (key, hasattr(self._field, key))
         if not is_all_set:
-            exception = "Gird is not all set:\n"
-            for key, value in self._requirement.items():
-                exception += "- %s: %s; " % (key, hasattr(self._field, key))
-                if value["require_gradient"]:
-                    exception += "gradient: %s; " % (hasattr(self._gradient, key),)
-                if value["require_curvature"]:
-                    exception += "curvature: %s; " % (hasattr(self._curvature, key),)
-                exception += "\n"
             raise GridPoorDefinedError(exception[:-1])
 
     def _check_shape(self, value: cp.ndarray, target_shape: list[int]):
         shape = value.shape
-        if len(shape) != self._num_dimensions:
-            raise ArrayDimError(
-                "A %d-D array is required, while %d-D is provided"
-                % (self._num_dimensions, len(shape))
+        exception = (
+            "Require Array with shape %s, while array with shape %s is provided"
+            % (
+                tuple(target_shape),
+                shape,
             )
+        )
+        if len(shape) != self._num_dimensions:
+            raise ArrayDimError(exception)
         for dim1, dim2 in zip(shape, target_shape):
             if dim1 != dim2:
-                raise ArrayDimError(
-                    "Require Array in %s, while %s is provided"
-                    % (tuple(target_shape), shape)
-                )
+                raise ArrayDimError(exception)
 
-    def add_field(
-        self,
-        name: str,
-        value: cp.ndarray,
-        require_gradient: bool = False,
-        require_curvature: bool = False,
-    ):
+    def add_field(self, name: str, value: cp.ndarray):
         # Set field
         self._check_shape(value, self._shape)
         setattr(self._field, name, value)
-        # Set gradient and curvature
-        if not name in self._requirement.keys():  # not required
-            self._requirement[name] = {
-                "require_gradient": False,
-                "require_curvature": False,
-            }
-        self._requirement[name]["require_gradient"] |= require_gradient
-        self._requirement[name]["require_curvature"] |= require_curvature
-        if self._requirement[name]["require_gradient"]:
-            setattr(
-                self._gradient, name, self.get_gradient(getattr(self._field, name)),
-            )
-        if self._requirement[name]["require_curvature"]:
-            setattr(
-                self._curvature, name, self.get_curvature(getattr(self._field, name)),
-            )
+
+    def add_constant(self, name: str, value: float):
+        setattr(self._constant, name, CUPY_FLOAT(value))
 
     def zeros_field(self, dtype=CUPY_FLOAT):
         return cp.zeros(self._shape, dtype)
 
     def ones_field(self, dtype=CUPY_FLOAT):
         return cp.ones(self._shape, dtype)
-
-    def get_gradient(self, field: cp.ndarray):
-        self._check_shape(field, self._shape)
-        gradient = cp.zeros([self._num_dimensions] + self._inner_shape, field.dtype)
-        slice_list = [slice(1, -1) for i in range(self.num_dimensions)]
-        for i in range(self._num_dimensions):
-            slice_list[i] = slice(2, self._shape[i])
-            gradient[i, :, :, :] = field[tuple(slice_list)]
-            slice_list[i] = slice(0, -2)
-            gradient[i, :, :, :] -= field[tuple(slice_list)]
-            gradient[i, :, :, :] /= 2 * self._grid_width[i]
-            slice_list[i] = slice(1, -1)
-        return gradient
-
-    def get_curvature(self, field: cp.ndarray):
-        self._check_shape(field, self._shape)
-        curvature = cp.zeros([self._num_dimensions] + self._inner_shape, field.dtype)
-        slice_list = [slice(1, -1) for i in range(self.num_dimensions)]
-        for i in range(self._num_dimensions):
-            slice_list[i] = slice(2, self._shape[i])
-            curvature[i, :, :, :] = field[tuple(slice_list)]
-            slice_list[i] = slice(0, -2)
-            curvature[i, :, :, :] += field[tuple(slice_list)]
-            slice_list[i] = slice(1, -1)
-            curvature[i, :, :, :] -= 2 * field[tuple(slice_list)]
-            curvature[i, :, :, :] /= self._grid_width[i] ** 2
-        return curvature
 
     @property
     def requirement(self) -> dict:
@@ -207,21 +146,14 @@ class Grid:
         return self._field
 
     @property
-    def gradient(self) -> object:
-        return self._gradient
-
-    @property
-    def curvature(self) -> object:
-        return self._curvature
+    def constant(self) -> object:
+        return self._constant
 
 
 if __name__ == "__main__":
-    grid = Grid(x=[-2, 2, 128], y=[-2, 2, 128], z=[-2, 2, 64])
+    grid = Grid(grid_width=0.1, x=[-2, 2], y=[-2, 2], z=[-2, 2])
     grid.set_requirement(
-        {
-            "phi": {"require_gradient": False, "require_curvature": False},
-            "epsilon": {"require_gradient": True, "require_curvature": True},
-        },
+        field_name_list=["phi", "epsilon"], constant_name_list=["epsilon0"]
     )
     print(grid.requirement)
     phi = grid.zeros_field()
@@ -229,7 +161,7 @@ if __name__ == "__main__":
 
     phi[0, :, :] = 20
     phi[-1, :, :] = 0
-    grid.add_field("phi", phi)
+    grid.add_field("phi", phi[1:-1, :, 1])
     grid.add_field("epsilon", epsilon)
     grid.check_requirement()
-    print(grid.gradient.phi)
+    print(grid.field.phi)
