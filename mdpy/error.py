c4640c3b7f1a11ee752ea921de2d08e975e949a2
zhenyuwei <zhenyuwei99@gmail.com>
2026-05-25 19:27:39 +0800

    chore: remove 10 unused exception classes from error.py

--- mdpy/error.py
+++ mdpy/error.py
@@ -25,15 +25,6 @@ class UnitDimensionDismatchedError(Exception):
     '''
     pass
 
-class GeomtryDimError(Exception):
-    '''This error occurs when:
-    - The dimension of geometry, like bond, angle, is mismatched
-
-    Used in:
-    - mdpy.core.topology
-    '''
-    pass
-
 class ArrayDimError(Exception):
     '''This error occurs when:
     - The dimension of argument does not meet the requirement
@@ -49,47 +40,12 @@ class ArrayDimError(Exception):
     '''
     pass
 
-class ParticleConflictError(Exception):
-    '''This error occurs when:
-    - Particle is twice bounded to a Particle instance
-    - Particle is bounded to itself
-    - Particle is twice bounded to a Toplogy instance
-    - Particle appears twice in bond, angle, dihedral or improper
-    - The number of particles is mismatched with the dimension of positions, velocities, forces matrix
-
-    Used in:
-    - mdpy.core.particle
-    - mdpy.core.topology
-    - mdpy.core.state
-    - mdpy.core.trajectory
-    - mdpy.ensemble
-    '''
-    pass
-
-class ConstraintConflictError(Exception):
-    '''This error occurs when:
-    - Constraint is twice bounded to a Ensemble instance
-
-    Used in:
-    - mdpy.ensemble
-    '''
-    pass
-
 class ModifyJoinedTopologyError(Exception):
     '''This error occurs when:
     - Adding particle or topology geometry to a joined Topology object
 
     Used in:
     - mdpy.core.topology
-    '''
-    pass
-
-class NonBoundedError(Exception):
-    '''This error occurs when:
-    - Parent object is not bounded
-
-    Used in:
-    - mdpy.constraint.constraint
     '''
     pass
 
@@ -120,40 +76,12 @@ class PBCPoorDefinedError(Exception):
     '''
     pass
 
-class CellListPoorDefinedError(Exception):
-    '''This error occurs when:
-    - The pbc info of cell list is not defined well
-    - The cutoff_radius of cell list is not defined well
-
-    Used in:
-    - mdpy.core.cell_list
-    '''
-    pass
-
-class TrajectoryPoorDefinedError(Exception):
-    '''This error occurs when:
-    - Extrating information that have not been contained
-
-    Used in:
-    -mdpy.core.trajectory
-    '''
-    pass
-
 class ParameterPoorDefinedError(Exception):
     '''This error occurs when:
     - Topology connections' parameter is not defined in selected parameter file
 
     Used in:
     - mdpy.forcefield.charmm_forcefield
-    '''
-    pass
-
-class HDF5FilePoorDefinedError(Exception):
-    '''This error occurs when:
-    - Use hdf5 file that does not meet the requirement of mdpy
-
-    Used in:
-    - mdpy.io.hdf5_parser
     '''
     pass
 
@@ -165,38 +93,6 @@ class ParserPoorDefinedError(Exception):
     - mdpy.io.hdf5_parser
     - mdpy.io.pdb_parser
     - mdpy.io.dcd_parser
-    '''
-    pass
-
-class DumperPoorDefinedError(Exception):
-    '''This error occurs when:
-    - Dump frequency of dumper object is 0
-    - Simulation integrates without adding dumper
-    - LogDumper requires rest_time without providing total_step
-
-    Used in:
-    - mdpy.dumper.dumper
-    - mdpy.dumper.log_dumper
-    - mdpy.simulation
-    '''
-    pass
-
-class SelectionConditionPoorDefinedError(Exception):
-    '''This error occurs when:
-    - Unsupported selection condition has been used
-
-    Used in:
-    - mdpy.utils.select_particle
-    '''
-    pass
-
-class AnalyserPoorDefinedError(Exception):
-    '''This error occurs when:
-    - Analyser's input data does not meet analyser's initial setting
-
-    Used in:
-    - mdpy.analyser.analyser_result
-    - mdpy.analyser.rmsd_analyser
     '''
     pass
 
