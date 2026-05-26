ef3091c4ae48d5499b6565e86789f325918355b9
zhenyuwei <zhenyuwei99@gmail.com>
2026-05-26 13:22:03 +0800

    chore(test): remove dead bind_sorted_params call from OpenMM validation

--- mdpy/test/test_openmm_validation.py
+++ mdpy/test/test_openmm_validation.py
@@ -49,9 +49,6 @@ def _setup_mdpy_system(psf_path, pdb_path, prm_path, cutoff=12.0):
         positions_2d, topology,
         system.pbc_matrix, system.pbc_inv,
     )
-    for term in system.force_terms:
-        if hasattr(term, 'bind_sorted_params'):
-            term.bind_sorted_params(system.tile_list)
     system.tile_list.update_sorted_positions(
         system.gpu.d_positions_x,
         system.gpu.d_positions_y,
