a47e370c9c4abfed3cac77a357544ca1a6317ff5
zhenyuwei <zhenyuwei99@gmail.com>
2026-05-26 14:16:27 +0800

    Change if else for energy dump, a flag here

--- mdpy/system.py
+++ mdpy/system.py
@@ -12,7 +12,9 @@ from mdpy.core.gpu_context import GPUContext
 
 class System:
 
-    def __init__(self, topology, pbc_matrix, cutoff=12.0, skin=1.0, rebuild_check_interval=10):
+    def __init__(
+        self, topology, pbc_matrix, cutoff=12.0, skin=1.0, rebuild_check_interval=10
+    ):
         self.topology = topology
         self.particles = ParticleTable(topology.num_particles)
         self.pbc_matrix = np.ascontiguousarray(pbc_matrix, dtype=env.NUMPY_FLOAT)
@@ -22,7 +24,9 @@ class System:
         self.gpu = GPUContext()
         self.gpu.initialize(topology, self.pbc_matrix.flatten())
 
-        self.tile_list = TileList(cutoff, skin=skin, rebuild_check_interval=rebuild_check_interval)
+        self.tile_list = TileList(
+            cutoff, skin=skin, rebuild_check_interval=rebuild_check_interval
+        )
         self.force_terms = []
 
         self._step_count = 0
@@ -36,7 +40,6 @@ class System:
         self._d_cached_unique_scale = None
         self._steps_since_check = 0
 
-
     def add_force_term(self, term):
         self.force_terms.append(term)
         self.gpu.allocate_energy_accumulator(len(self.force_terms))
@@ -46,7 +49,7 @@ class System:
         self._profile_data = {}
         for term in self.force_terms:
             self._profile_data[term.name] = []
-        self._profile_data['integrator'] = []
+        self._profile_data["integrator"] = []
 
     def disable_profiling(self):
         self._profiling_enabled = False
@@ -58,9 +61,9 @@ class System:
             if pairs:
                 times = [cp.cuda.get_elapsed_time(s, e) for s, e in pairs]
                 result[key] = {
-                    'total_ms': sum(times),
-                    'avg_ms': sum(times) / len(times),
-                    'count': len(times),
+                    "total_ms": sum(times),
+                    "avg_ms": sum(times) / len(times),
+                    "count": len(times),
                 }
         self._profile_data = {k: [] for k in self._profile_data}
         return result
@@ -68,18 +71,18 @@ class System:
     def compute_forces(self):
         self.gpu.zero_forces()
         self.tile_list.update_sorted_positions(
-            self.gpu.d_positions_x,
-            self.gpu.d_positions_y,
-            self.gpu.d_positions_z)
+            self.gpu.d_positions_x, self.gpu.d_positions_y, self.gpu.d_positions_z
+        )
         for term_index, term in enumerate(self.force_terms):
             if self._profiling_enabled:
                 s = cp.cuda.Event()
                 e = cp.cuda.Event()
                 s.record()
-            if self._compute_energy:
+            if self._compute_energy == False:
+                term.compute(self.gpu, self.tile_list)
+            else:
                 self.gpu.zero_energy()
-            term.compute(self.gpu, self.tile_list)
-            if self._compute_energy:
+                term.compute(self.gpu, self.tile_list)
                 self.gpu.accumulate_energy(term_index)
             if self._profiling_enabled:
                 e.record()
@@ -109,23 +112,24 @@ class System:
         perm_gpu = tl.d_raw_order
 
         for old_arr, name in [
-            (gpu.d_positions_x, 'd_positions_x'),
-            (gpu.d_positions_y, 'd_positions_y'),
-            (gpu.d_positions_z, 'd_positions_z'),
-            (gpu.d_velocities_x, 'd_velocities_x'),
-            (gpu.d_velocities_y, 'd_velocities_y'),
-            (gpu.d_velocities_z, 'd_velocities_z'),
-            (gpu.d_forces_x, 'd_forces_x'),
-            (gpu.d_forces_y, 'd_forces_y'),
-            (gpu.d_forces_z, 'd_forces_z'),
-            (gpu.d_prev_positions_x, 'd_prev_positions_x'),
-            (gpu.d_prev_positions_y, 'd_prev_positions_y'),
-            (gpu.d_prev_positions_z, 'd_prev_positions_z'),
-            (gpu.d_masses, 'd_masses'),
+            (gpu.d_positions_x, "d_positions_x"),
+            (gpu.d_positions_y, "d_positions_y"),
+            (gpu.d_positions_z, "d_positions_z"),
+            (gpu.d_velocities_x, "d_velocities_x"),
+            (gpu.d_velocities_y, "d_velocities_y"),
+            (gpu.d_velocities_z, "d_velocities_z"),
+            (gpu.d_forces_x, "d_forces_x"),
+            (gpu.d_forces_y, "d_forces_y"),
+            (gpu.d_forces_z, "d_forces_z"),
+            (gpu.d_prev_positions_x, "d_prev_positions_x"),
+            (gpu.d_prev_positions_y, "d_prev_positions_y"),
+            (gpu.d_prev_positions_z, "d_prev_positions_z"),
+            (gpu.d_masses, "d_masses"),
         ]:
             new_arr = cp.empty_like(old_arr)
-            tl._kernels['permute'](grid, (tpb,),
-                (old_arr, perm_gpu, np.int32(N), new_arr))
+            tl._kernels["permute"](
+                grid, (tpb,), (old_arr, perm_gpu, np.int32(N), new_arr)
+            )
             setattr(gpu, name, new_arr)
 
         d_remap = tl.d_pdb_to_sorted
@@ -137,7 +141,9 @@ class System:
                 self._d_cached_unique_i,
                 self._d_cached_unique_j,
                 self._d_cached_unique_scale,
-                d_composed_perm, N)
+                d_composed_perm,
+                N,
+            )
             d_excl_offset = result[0]
             d_excl_neighbors = result[1]
             d_excl_scale = result[2]
@@ -146,11 +152,18 @@ class System:
             self._d_cached_unique_scale = result[5]
         else:
             remap_np = cp.asnumpy(d_remap)
-            for field in ('bond_indices', 'angle_indices', 'dihedral_indices', 'improper_indices'):
+            for field in (
+                "bond_indices",
+                "angle_indices",
+                "dihedral_indices",
+                "improper_indices",
+            ):
                 indices = getattr(self.topology, field, None)
                 if indices is not None and len(indices) > 0:
                     setattr(self.topology, field, remap_np[indices])
-            d_excl_offset, d_excl_neighbors, d_excl_scale, d_unique_i = build_exclusion_map_gpu(self.topology, scale_14=1.0)
+            d_excl_offset, d_excl_neighbors, d_excl_scale, d_unique_i = (
+                build_exclusion_map_gpu(self.topology, scale_14=1.0)
+            )
             self._d_cached_unique_i = d_unique_i
             self._d_cached_unique_j = d_excl_neighbors
             self._d_cached_unique_scale = d_excl_scale
@@ -158,7 +171,7 @@ class System:
         tl.set_gpu_exclusion(d_excl_offset, d_excl_neighbors, d_excl_scale)
 
         for term in self.force_terms:
-            if hasattr(term, 'remap_indices_gpu'):
+            if hasattr(term, "remap_indices_gpu"):
                 term.remap_indices_gpu(d_remap)
 
     def step(self, integrator, number_steps=1):
@@ -182,7 +195,7 @@ class System:
             integrator.step(self.gpu)
             if prof:
                 e.record()
-                self._profile_data['integrator'].append((s, e))
+                self._profile_data["integrator"].append((s, e))
             self._step_count += 1
             self._steps_since_check += 1
             if self._steps_since_check >= self.tile_list.rebuild_check_interval:
@@ -192,13 +205,15 @@ class System:
 
     def _do_full_rebuild(self, positions_soa):
         self.tile_list.rebuild(
-            positions_soa, self.topology,
-            self.pbc_matrix, self.pbc_inv,
+            positions_soa,
+            self.topology,
+            self.pbc_matrix,
+            self.pbc_inv,
         )
         self._permute_all_arrays()
         self.tile_list.build_tiles(self.topology, self.pbc_matrix)
         for term in self.force_terms:
-            if hasattr(term, 'bind_sorted'):
+            if hasattr(term, "bind_sorted"):
                 term.bind_sorted(self.topology, self.tile_list, self.gpu)
 
     def minimize(self, minimizer, number_steps=100):
@@ -212,13 +227,15 @@ class System:
         positions_soa = self.gpu.get_positions_2d()
         if self.tile_list.check_rebuild(positions_soa):
             self.tile_list.rebuild(
-                positions_soa, self.topology,
-                self.pbc_matrix, self.pbc_inv,
+                positions_soa,
+                self.topology,
+                self.pbc_matrix,
+                self.pbc_inv,
             )
             self._permute_all_arrays()
             self.tile_list.build_tiles(self.topology, self.pbc_matrix)
         for term in self.force_terms:
-            if hasattr(term, 'bind_sorted'):
+            if hasattr(term, "bind_sorted"):
                 term.bind_sorted(self.topology, self.tile_list, self.gpu)
         self.compute_forces()
         for _ in range(number_steps):
