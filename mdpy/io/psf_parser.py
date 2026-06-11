04cc377c52328c765c69558231370bc6a3b38cbf
zhenyuwei <zhenyuwei99@gmail.com>
2026-06-11 12:28:20 +0800

    refactor: rewrite PSFParser with pure text parsing, remove MDAnalysis dependency

--- mdpy/io/psf_parser.py
+++ mdpy/io/psf_parser.py
@@ -7,55 +7,120 @@ author : Zhenyu Wei
 copyright : (C)Copyright 2021-present, mdpy organization
 '''
 
-import warnings
 import numpy as np
-import MDAnalysis as mda
 from mdpy import env
 from mdpy.core.topology import Topology, Builder
 from mdpy.error import *
+
 
 class PSFParser:
     def __init__(self, file_path: str) -> None:
         if not file_path.endswith('.psf'):
             raise FileFormatError('The file should end with .psf suffix')
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            self._parser = mda.topology.PSFParser.PSFParser(file_path).parse()
-        self._num_particles = self._parser.n_atoms
-        self._particle_ids = list(self._parser.ids.values)
-        self._type_names = list(self._parser.types.values)
-        self._particle_names = list(self._parser.names.values)
+        self._file_path = file_path
+        with open(file_path, 'r') as f:
+            lines = f.readlines()
+        self._parse(lines)
 
-        molecule_ids = self._parser.resids.values
-        molecule_types = self._parser.resnames.values
-        chain_ids = self._parser.segids.values
+    def _parse(self, lines):
+        idx = 0
+        idx = self._skip_header(lines, idx)
+        idx = self._parse_atoms(lines, idx)
+        idx = self._parse_section(lines, idx, '!NBOND:', 2, '_bonds')
+        idx = self._parse_section(lines, idx, '!NTHETA:', 3, '_angles')
+        idx = self._parse_section(lines, idx, '!NPHI:', 4, '_dihedrals')
+        idx = self._parse_section(lines, idx, '!NIMPHI:', 4, '_impropers')
+        self._build_type_index()
+        self._topology = self._create_topology()
 
+    def _skip_header(self, lines, idx):
+        while idx < len(lines):
+            stripped = lines[idx].strip()
+            if stripped.startswith('!NTITLE') or stripped.endswith('!NTITLE'):
+                parts = stripped.split()
+                n_title = int(parts[0])
+                return idx + 1 + n_title
+            idx += 1
+        raise ParserPoorDefinedError('PSF file missing !NTITLE section')
+
+    def _parse_atoms(self, lines, idx):
+        while idx < len(lines):
+            stripped = lines[idx].strip()
+            if not stripped:
+                idx += 1
+                continue
+            parts = stripped.split()
+            if '!NATOM' in stripped:
+                n_atoms = int(parts[0])
+                idx += 1
+                break
+            idx += 1
+        else:
+            raise ParserPoorDefinedError('PSF file missing !NATOM section')
+
+        self._num_particles = n_atoms
+        self._particle_ids = []
+        self._type_names = []
+        self._particle_names = []
         self._molecule_ids = []
         self._molecule_types = []
         self._chain_ids = []
-        for i in range(self._num_particles):
-            resid = self._parser.tt.atoms2residues(i)
-            segid = self._parser.tt.atoms2segments(i)
-            self._molecule_ids.append(int(molecule_ids[resid]))
-            self._molecule_types.append(molecule_types[resid])
-            self._chain_ids.append(chain_ids[segid])
+        self._masses = np.zeros(n_atoms, dtype=env.NUMPY_FLOAT)
+        self._charges = np.zeros(n_atoms, dtype=env.NUMPY_FLOAT)
 
-        self._masses = np.array(self._parser.masses.values, dtype=env.NUMPY_FLOAT)
-        self._charges = np.array(self._parser.charges.values, dtype=env.NUMPY_FLOAT)
+        for i in range(n_atoms):
+            parts = lines[idx + i].split()
+            self._particle_ids.append(int(parts[0]))
+            self._chain_ids.append(parts[1])
+            self._molecule_ids.append(int(parts[2]))
+            self._molecule_types.append(parts[3])
+            self._particle_names.append(parts[4])
+            self._type_names.append(parts[5])
+            self._charges[i] = float(parts[6])
+            self._masses[i] = float(parts[7])
 
-        self._bonds = [list(i) for i in self._parser.bonds.values]
-        self._angles = [list(i) for i in self._parser.angles.values]
-        self._dihedrals = [list(i) for i in self._parser.dihedrals.values]
-        self._impropers = [list(i) for i in self._parser.impropers.values]
+        return idx + n_atoms
 
+    def _parse_section(self, lines, idx, marker, width, attr_name):
+        while idx < len(lines):
+            stripped = lines[idx].strip()
+            if not stripped:
+                idx += 1
+                continue
+            if marker in stripped:
+                parts = stripped.split()
+                count = int(parts[0])
+                idx += 1
+                break
+            idx += 1
+        else:
+            setattr(self, attr_name, [])
+            return idx
+
+        all_ints = []
+        while len(all_ints) < count * width and idx < len(lines):
+            stripped = lines[idx].strip()
+            if not stripped:
+                idx += 1
+                continue
+            if stripped.startswith('!'):
+                break
+            all_ints.extend(int(x) for x in stripped.split())
+            idx += 1
+
+        interactions = []
+        for i in range(0, count * width, width):
+            interactions.append([all_ints[i + j] - 1 for j in range(width)])
+        setattr(self, attr_name, interactions)
+        return idx
+
+    def _build_type_index(self):
         unique_types = sorted(set(self._type_names))
         self._type_name_to_index = {name: idx for idx, name in enumerate(unique_types)}
         self._particle_type_indices = np.array(
             [self._type_name_to_index[t] for t in self._type_names], dtype=env.NUMPY_INT
         )
         self._unique_type_names = unique_types
-
-        self._topology = self._create_topology()
 
     def _create_topology(self):
         builder = Builder()
