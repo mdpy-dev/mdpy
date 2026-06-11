3a500f1e547de37ff3dea06682c54919fccbb50c
zhenyuwei <zhenyuwei99@gmail.com>
2026-06-11 12:31:51 +0800

    refactor: rewrite PDBParser with pure text parsing, remove MDAnalysis dependency

--- mdpy/io/pdb_parser.py
+++ mdpy/io/pdb_parser.py
@@ -7,45 +7,115 @@ author : Zhenyu Wei
 copyright : (C)Copyright 2021-present, mdpy organization
 '''
 
-import warnings
 import numpy as np
-import MDAnalysis as mda
-from MDAnalysis.topology.guessers import guess_atom_type
 from mdpy import env, SPATIAL_DIM
 from mdpy.error import FileFormatError, ArrayDimError, ParserPoorDefinedError
 
+
+def _guess_element(atom_name):
+    name = atom_name.strip()
+    if not name:
+        return ''
+    first = name[0]
+    if first.isdigit():
+        return name[1] if len(name) > 1 else ''
+    return first.upper()
+
+
 class PDBParser:
     def __init__(self, file_path, is_parse_all=True) -> None:
-        # Initial reader and parser setting
         if not file_path.endswith('.pdb'):
             raise FileFormatError('The file should end with .pdb suffix')
         self._file_path = file_path
         self._is_parse_all = is_parse_all
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            self._reader = mda.coordinates.PDB.PDBReader(self._file_path)
-            self._parser = mda.topology.PDBParser.PDBParser(self._file_path).parse()
-        # Parse data
-        self._num_particles = self._parser.n_atoms
-        self._particle_ids = list(self._parser.ids.values)
-        self._particle_types = list(map(guess_atom_type, self._parser.names.values))
-        self._particle_names = list(self._parser.names.values)
-        self._matrix_ids = list(np.linspace(0, self._num_particles-1, self._num_particles, dtype=env.NUMPY_INT))
-        molecule_ids, molecule_types = self._parser.resids.values, self._parser.resnames.values
-        self._molecule_ids, self._molecule_types = [], []
-        for i in range(self._parser.n_atoms):
-            resid = self._parser.tt.atoms2residues(i)
-            self._molecule_ids.append(molecule_ids[resid])
-            self._molecule_types.append(molecule_types[resid])
-        self._chain_ids = list(self._parser.chainIDs.values)
-        self._num_frames = self._reader.trajectory.n_frames
+        with open(file_path, 'r') as f:
+            lines = f.readlines()
+        self._parse(lines)
+
+    def _parse(self, lines):
+        frames = []
+        current_frame = []
+
+        self._particle_ids = []
+        self._particle_types = []
+        self._particle_names = []
+        self._molecule_ids = []
+        self._molecule_types = []
+        self._chain_ids = []
+        self._matrix_ids = []
+        self._pbc_matrix = None
+
+        atom_index = 0
+        first_atom_section = True
+
+        for line in lines:
+            record = line[:6].strip() if len(line) >= 6 else line.strip()
+
+            if record == 'CRYST1' and self._pbc_matrix is None:
+                self._pbc_matrix = self._parse_cryst1(line)
+
+            if record in ('ATOM', 'HETATM'):
+                if first_atom_section:
+                    self._particle_ids.append(int(line[6:11]))
+                    self._particle_names.append(line[12:16].strip())
+                    self._particle_types.append(_guess_element(line[12:16]))
+                    self._molecule_ids.append(int(line[22:26]))
+                    self._molecule_types.append(line[17:21].strip())
+                    self._chain_ids.append(line[21])
+                    self._matrix_ids.append(atom_index)
+                    atom_index += 1
+
+                x = float(line[30:38])
+                y = float(line[38:46])
+                z = float(line[46:54])
+                current_frame.append([x, y, z])
+
+            elif record == 'ENDMDL':
+                if current_frame:
+                    frames.append(np.array(current_frame, dtype=env.NUMPY_FLOAT))
+                    current_frame = []
+                    first_atom_section = False
+
+            elif record == 'END':
+                if current_frame:
+                    frames.append(np.array(current_frame, dtype=env.NUMPY_FLOAT))
+                    current_frame = []
+                    first_atom_section = False
+
+        if current_frame:
+            frames.append(np.array(current_frame, dtype=env.NUMPY_FLOAT))
+
+        self._num_particles = len(self._particle_ids)
+        self._num_frames = len(frames)
+
         if self._is_parse_all:
             if self._num_frames == 1:
-                self._positions = self._reader.ts.positions.astype(env.NUMPY_FLOAT)
+                self._positions = frames[0] if frames else np.zeros((0, SPATIAL_DIM), dtype=env.NUMPY_FLOAT)
             else:
-                self._positions = [ts.positions.astype(env.NUMPY_FLOAT) for ts in self._reader.trajectory]
-                self._positions = np.stack(self._positions)
-        self._pbc_matrix = self._reader.ts.triclinic_dimensions
+                self._positions = np.stack(frames) if frames else np.zeros((0, self._num_particles, SPATIAL_DIM), dtype=env.NUMPY_FLOAT)
+        else:
+            self._positions = None
+            self._frames = frames
+
+    @staticmethod
+    def _parse_cryst1(line):
+        a = float(line[6:15])
+        b = float(line[15:24])
+        c = float(line[24:33])
+        alpha = float(line[33:40])
+        beta = float(line[40:47])
+        gamma = float(line[47:54])
+        alpha_r = np.radians(alpha)
+        beta_r = np.radians(beta)
+        gamma_r = np.radians(gamma)
+        box = np.zeros((3, 3))
+        box[0, 0] = a
+        box[1, 0] = b * np.cos(gamma_r)
+        box[1, 1] = b * np.sin(gamma_r)
+        box[2, 0] = c * np.cos(beta_r)
+        box[2, 1] = c * (np.cos(alpha_r) - np.cos(beta_r) * np.cos(gamma_r)) / np.sin(gamma_r)
+        box[2, 2] = np.sqrt(c * c - box[2, 0] ** 2 - box[2, 1] ** 2)
+        return box
 
     def get_matrix_id(self, particle_id):
         return self._particle_ids.index(particle_id)
@@ -62,26 +132,45 @@ class PDBParser:
             'matrix_id': matrix_id,
             'position': self._positions[matrix_id, :]
         }
-    
+
     def get_positions(self, *frames):
         num_target_frames = len(frames)
-        if num_target_frames == 1:
-            if frames[0] >= self._num_frames:
-                raise ArrayDimError(
-                    '%d beyond the number of frames %d stored in pdb file'
-                    %(frames[0], self._num_frames)
-                )
-            result = self._reader.trajectory[frames[0]].positions.copy().astype(env.NUMPY_FLOAT)
-        else:
-            result = np.zeros([num_target_frames, self._num_particles, SPATIAL_DIM])
-            for index, frame in enumerate(frames):
-                if frame >= self._num_frames:
+        if self._is_parse_all:
+            if num_target_frames == 1:
+                if frames[0] >= self._num_frames:
                     raise ArrayDimError(
                         '%d beyond the number of frames %d stored in pdb file'
-                        %(frame, self._num_frames)
+                        %(frames[0], self._num_frames)
                     )
-                result[index, :, :] = self._reader.trajectory[frame].positions.astype(env.NUMPY_FLOAT)
-        return result
+                return self._positions[frames[0]].copy() if self._num_frames > 1 else self._positions.copy()
+            else:
+                result = np.zeros([num_target_frames, self._num_particles, SPATIAL_DIM])
+                for index, frame in enumerate(frames):
+                    if frame >= self._num_frames:
+                        raise ArrayDimError(
+                            '%d beyond the number of frames %d stored in pdb file'
+                            %(frame, self._num_frames)
+                        )
+                    result[index, :, :] = self._positions[frame]
+                return result
+        else:
+            if num_target_frames == 1:
+                if frames[0] >= self._num_frames:
+                    raise ArrayDimError(
+                        '%d beyond the number of frames %d stored in pdb file'
+                        %(frames[0], self._num_frames)
+                    )
+                return self._frames[frames[0]].copy()
+            else:
+                result = np.zeros([num_target_frames, self._num_particles, SPATIAL_DIM])
+                for index, frame in enumerate(frames):
+                    if frame >= self._num_frames:
+                        raise ArrayDimError(
+                            '%d beyond the number of frames %d stored in pdb file'
+                            %(frame, self._num_frames)
+                        )
+                    result[index, :, :] = self._frames[frame]
+                return result
 
     @property
     def particle_ids(self):
@@ -110,11 +199,11 @@ class PDBParser:
     @property
     def num_frames(self):
         return self._num_frames
-    
+
     @property
     def num_particles(self):
         return self._num_particles
-    
+
     @property
     def positions(self) -> np.ndarray:
         if not self._is_parse_all:
