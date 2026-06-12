2d26e3b50cc30a6c2c29f87ed200937a545db20d
zhenyuwei <zhenyuwei99@gmail.com>
2026-06-12 22:23:39 +0800

    feat: export new expression API symbols

--- mdpy/force/__init__.py
+++ mdpy/force/__init__.py
@@ -1,3 +1,6 @@
 from mdpy.force.force_term import ForceTerm
+from mdpy.force.markers import param, scalar
+from mdpy.force.bonded_transpiler import bonded_expression
+from mdpy.force.nonbonded_transpiler import nonbonded_expression
 
-__all__ = ['ForceTerm']
+__all__ = ['ForceTerm', 'param', 'scalar', 'bonded_expression', 'nonbonded_expression']
