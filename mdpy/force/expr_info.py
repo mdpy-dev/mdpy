51186c084d2adf5ec93f28eb52f382bbff527f56
zhenyuwei <zhenyuwei99@gmail.com>
2026-06-13 19:15:25 +0800

    refactor: remove dead classify_arguments and particle_properties from expr_info

--- mdpy/force/expr_info.py
+++ mdpy/force/expr_info.py
@@ -1,6 +1,4 @@
-import inspect
 import re
-from mdpy.force.markers import param as _param_marker, scalar as _scalar_marker
 
 
 class ExprInfo:
@@ -11,10 +9,6 @@ class ExprInfo:
         self.scalars = scalars
         self.body = body
 
-    @property
-    def particle_properties(self):
-        return set(self.per_particle.values())
-
 
 def _strip_trailing_digits(name):
     m = re.match(r'^(.*?)(\d+)$', name)
@@ -24,24 +18,3 @@ def _strip_trailing_digits(name):
     if base.endswith('_'):
         base = base[:-1]
     return base
-
-
-def classify_arguments(func, body):
-    sig = inspect.signature(func)
-    positions = []
-    per_particle = {}
-    params = []
-    scalars = []
-
-    for i, (name, p) in enumerate(sig.parameters.items()):
-        if i < body and re.match(r'^pos\d+$', name):
-            positions.append(name)
-        elif p.default is _param_marker:
-            params.append(name)
-        elif p.default is _scalar_marker:
-            scalars.append(name)
-        elif p.default is inspect.Parameter.empty:
-            prop_name = _strip_trailing_digits(name)
-            per_particle[name] = prop_name
-
-    return ExprInfo(positions, per_particle, params, scalars, body)
