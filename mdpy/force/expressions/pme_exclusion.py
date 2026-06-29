088c6c7611fcb09415f441455c7f53ebe67789ce
zhenyuwei <zhenyuwei99@gmail.com>
2026-06-29 15:30:41 +0800

    feat: add pme_exclusion_correction bonded expression

---
+++ mdpy/force/expressions/pme_exclusion.py
@@ -0,0 +1,14 @@
+import math
+
+from mdpy.unit import EPSILON0
+from mdpy.force.bonded_transpiler import bonded_expression
+from mdpy.force.primitives import param, distance
+
+# Coulomb constant 1/(4*pi*epsilon0) in mdpy internal units (file-local).
+COULOMB_CONST = 1.0 / (4.0 * math.pi * float(EPSILON0.value))
+
+
+@bonded_expression(body=2)
+def pme_exclusion_correction(pos1, pos2, charge1, charge2, alpha=param):
+    r = distance(pos1, pos2)
+    return -COULOMB_CONST * charge1 * charge2 * erf(alpha * r) / r
