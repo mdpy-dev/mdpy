91b26f407c96600c616deecd24a6ca4634eb6119
zhenyuwei <zhenyuwei99@gmail.com>
2026-07-01 17:06:20 +0800

    bench(ion): route PME term to dedicated stream

--- benchmark/benchmark_ion.py
+++ benchmark/benchmark_ion.py
@@ -46,7 +46,7 @@ system.upload_pbc(pbc_matrix)
 print(forces["bonded"])
 system.add_force_term(forces["bonded"])
 system.add_force_term(forces["nonbonded"])
-system.add_force_term(forces["pme"])
+system.add_force_term(forces["pme"], stream="pme")
 
 positions = pdb.positions
 velocities = generate_velocity_from_temperature(300.0, topology.masses, seed=42)
@@ -58,7 +58,7 @@ integrator = LangevinBAOABIntegrator(DT_FS, 300.0, 1.0)
 
 def _run_steps(n):
     for i in range(n):
-        system.update_neighbor_list(sync_interval=20)
+        system.update_neighbor_list(sync_interval=50)
         system.compute_forces()
         integrator.step(system)
 
